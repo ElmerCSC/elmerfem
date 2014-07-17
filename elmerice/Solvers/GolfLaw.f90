da36c1c7a6fb0ed14ba5798e05f2b78010fe6386
fgillet <fgillet@04224531-1f84-46d8-96ce-875b9e37dce3>
2014-07-17 13:26:42 +0000

    ElmerIce: GolfLaw.f90 fix computation of Euler angles
    git-svn-id: svn+ssh://svn.code.sf.net/p/elmerfem/code/trunk@6816 04224531-1f84-46d8-96ce-875b9e37dce3

--- elmerice/Solvers/GolfLaw.f90
+++ elmerice/Solvers/GolfLaw.f90
@@ -708,7 +708,7 @@
       Real(dp),dimension(6),intent(in) :: a2
       Real(dp),dimension(3),intent(out) :: ai,Euler
       Real(dp),dimension(3,3) :: A,EigenVec
-      Real(dp) :: Dumy(1),EI(1),Work(24)
+      Real(dp) :: Dumy(1,3),EI(3),Work(24)
       integer :: dim               ! dimension  (2D-3D)
       integer :: i,infor 
 
@@ -722,12 +722,20 @@
          A(1,3)=a2(6)
          A(3,1)=A(1,3)
       
+
       CALL DGEEV('N','V',3,A,3,ai,EI,Dumy,1,EigenVec,3,Work,24,infor)
 
+      if (infor.ne.0) &
+      CALL FATAL('GolfLaw,R2R0', 'failed to compute fabric eignevalues')
+
       Euler(2)=Acos(EigenVec(3,3))
-      Euler(1)=ATAN2(EigenVec(3,1),-EigenVec(3,2))
-      Euler(3)=ATAN2(EigenVec(1,3),EigenVec(2,3))
-      
+      if (abs(Euler(2)).gt.tiny(Euler(2))) then !3D euler angles 
+        Euler(1)=ATAN2(EigenVec(3,1),-EigenVec(3,2))
+        Euler(3)=ATAN2(EigenVec(1,3),EigenVec(2,3))
+      else ! only one rotation of angle phi
+        Euler(3)=0.0
+        Euler(1)=ATAN2(EigenVec(2,1),EigenVec(1,1))
+      end if
 
       RETURN
       END
