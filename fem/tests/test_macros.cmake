c5827b4e448bb1efac352c6b596bb01fc77991b8
Juhani Kataja <juhani.kataja@csc.fi>
2014-11-14 13:58:20 +0200

    ctest: fixed path delimeter and mgdyn_steady_periodic test

--- fem/tests/test_macros.cmake
+++ fem/tests/test_macros.cmake
@@ -35,37 +35,28 @@ macro(ADD_ELMERTEST_MODULE test_name module_name file_name)
   UNSET(ELMERTEST_CMAKE_NAME)
 endmacro()
 
-#macro(RUN_ELMER_TEST)
-  #set(ENV{ELMER_HOME} "${BINARY_DIR}/fem/src")
-  #set(ENV{ELMER_LIB} "${BINARY_DIR}/fem/src/modules")
-  #set(ENV{PATH} "$ENV{PATH};${BINARY_DIR}/meshgen2d/src;c:/elmer_stuff/mingw64/bin;$ENV{ELMER_HOME};$ENV{ELMER_LIB};${BINARY_DIR}/fhutiter/src;${BINARY_DIR}/matc/src;${BINARY_DIR}/mathlibs/src/arpack")
-
-  ##MESSAGE(STATUS "\$Env:path=\"$ENV{PATH}\"")
-  #execute_process(COMMAND ${ELMERSOLVER_BIN} OUTPUT_FILE "elmersolver-stdout.log"
-    #ERROR_FILE "elmersolver-stderr.log" OUTPUT_VARIABLE TESTOUTPUT)
-  #execute_process(COMMAND ${FINDNORM_BIN} ${CMAKE_CURRENT_BINARY_DIR}/elmersolver-stdout.log
-    #OUTPUT_FILE "findnorm-stdout.log" ERROR_FILE "findnorm-stderr.log" RESULT_VARIABLE RES OUTPUT_VARIABLE FINDNORM_OUTPUT)
-
-  #MESSAGE(STATUS "findnorm res.......: ${RES}")
-  #MESSAGE(STATUS "testoutput.........: ${TESTOUTPUT}")
-  #MESSAGE(STATUS "findnorm output....: ${FINDNORM_OUTPUT}")
-  #if (RES)
-    #message(FATAL_ERROR "Test failed")
-  #endif()
-#endmacro()
-
 macro(RUN_ELMER_TEST)
+  MESSAGE(STATUS "BINARY_DIR = ${BINARY_DIR}")
   set(ENV{ELMER_HOME} "${BINARY_DIR}/fem/src")
   set(ENV{ELMER_LIB} "${BINARY_DIR}/fem/src/modules")
-  set(ENV{PATH} "$ENV{PATH};${BINARY_DIR}/meshgen2d/src/;${BINARY_DIR}/fem/src")
+
+  IF(NOT(WIN32))
+    set(ENV{PATH} "$ENV{PATH}:${BINARY_DIR}/meshgen2d/src/:${BINARY_DIR}/fem/src")
+  ENDIF(NOT(WIN32))
+
   FILE(REMOVE TEST.PASSED)
+
   IF(WIN32)
+    set(ENV{PATH} "$ENV{PATH};${BINARY_DIR}/meshgen2d/src/;${BINARY_DIR}/fem/src")
     GET_FILENAME_COMPONENT(COMPILER_DIRECTORY ${CMAKE_Fortran_COMPILER} PATH)
     set(ENV{PATH} "$ENV{PATH};${COMPILER_DIRECTORY};$ENV{ELMER_HOME};$ENV{ELMER_LIB};${BINARY_DIR}/fhutiter/src;${BINARY_DIR}/matc/src;${BINARY_DIR}/mathlibs/src/arpack")
   ENDIF(WIN32)
+
   execute_process(COMMAND ${ELMERSOLVER_BIN} OUTPUT_FILE "test-stdout.log"
     ERROR_FILE "test-stderr.log" OUTPUT_VARIABLE TESTOUTPUT)
+
   MESSAGE(STATUS "testoutput.........: ${TESTOUTPUT}")
+
   file(READ "TEST.PASSED" RES)
   if (NOT RES EQUAL "1")
     message(FATAL_ERROR "Test failed")
@@ -75,8 +66,13 @@ endmacro()
 macro(EXECUTE_ELMER_SOLVER SIFNAME)
   set(ENV{ELMER_HOME} "${BINARY_DIR}/fem/src")
   set(ENV{ELMER_LIB} "${BINARY_DIR}/fem/src/modules")
-  set(ENV{PATH} "$ENV{PATH}:${BINARY_DIR}/meshgen2d/src/:${BINARY_DIR}/fem/src")
+
+  IF(NOT(WIN32))
+    set(ENV{PATH} "$ENV{PATH}:${BINARY_DIR}/meshgen2d/src/:${BINARY_DIR}/fem/src")
+  ENDIF(NOT(WIN32))
+
   IF(WIN32)
+    set(ENV{PATH} "$ENV{PATH};${BINARY_DIR}/meshgen2d/src/;${BINARY_DIR}/fem/src")
     GET_FILENAME_COMPONENT(COMPILER_DIRECTORY ${CMAKE_Fortran_COMPILER} PATH)
     set(ENV{PATH} "${COMPILER_DIRECTORY};$ENV{ELMER_HOME};$ENV{ELMER_LIB};${BINARY_DIR}/fhutiter/src;${BINARY_DIR}/matc/src;${BINARY_DIR}/mathlibs/src/arpack")
   ENDIF(WIN32)
