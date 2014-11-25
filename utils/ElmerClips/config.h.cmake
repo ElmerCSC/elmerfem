d558cffb94e603d579c6932af107f54ad1ecec9c
Juhani Kataja <juhani.kataja@csc.fi>
2014-11-25 13:31:00 +0200

    elmerclips compiles on linux

---
+++ utils/ElmerClips/config.h.cmake
@@ -0,0 +1,1 @@
+#cmakedefine CodecID @CodecID@
--- utils/ElmerClips/src/encoder.cpp
+++ utils/ElmerClips/src/encoder.cpp
@@ -37,6 +37,7 @@
  *  Original Date: 14 Nov 2010                                               *
  *                                                                           *
  *****************************************************************************/
+#include "config.h"
 #include "encoder.h"
 
 Encoder::Encoder(QObject *parent) : QThread(parent), quality(2)
