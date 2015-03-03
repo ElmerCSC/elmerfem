c25659aeb95f286fac2931b94c977aaaae1e2095
mbycklin <mikko.byckling@csc.fi>
2015-03-03 10:28:29 +0200

    Added better support for cross-compilation. Added GNU toolchains for taito.csc.fi (still untested).

--- cmake/Toolchains/Elmer-linux-taito-gcc-hsw.cmake
+++ cmake/Toolchains/Elmer-linux-taito-gcc-hsw.cmake
@@ -1,28 +1,30 @@
-# CMake toolchain file for building on taito-mic.csc.fi
+# CMake toolchain file for building on taito.csc.fi
+# with the GNU toolchain and Intel MKL for the Haswell nodes
 #
 # Author: Mikko Byckling, CSC - IT Center for Science Ltd.
-# Version: 0.1
+# Version: 0.2
 
+# Cross-compile for Haswell nodes on taito.csc.fi
 SET(CMAKE_SYSTEM_NAME Linux)
-SET(CMAKE_SYSTEM_PROCESSOR x86_64)
+SET(CMAKE_SYSTEM_PROCESSOR k1om)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 # Specify the cross compilers (serial)
-SET(CMAKE_C_COMPILER icc)
-SET(CMAKE_Fortran_COMPILER ifort)
-SET(CMAKE_CXX_COMPILER icpc)
+SET(CMAKE_C_COMPILER gcc)
+SET(CMAKE_Fortran_COMPILER gfortran)
+SET(CMAKE_CXX_COMPILER g++)
 
 # Specify the cross compilers (parallel)
-SET(MPI_C_COMPILER mpiicc)
-SET(MPI_CXX_COMPILER mpiicpc)
-SET(MPI_Fortran_COMPILER mpiifort)
+SET(MPI_C_COMPILER mpicc)
+SET(MPI_CXX_COMPILER mpicxx)
+SET(MPI_Fortran_COMPILER mpif90)
 
 # Compilation flags (i.e. with optimization)
-SET(CMAKE_C_FLAGS "-O3 -g" CACHE STRING "")
-SET(CMAKE_CXX_FLAGS "-O3 -g" CACHE STRING "")
-SET(CMAKE_Fortran_FLAGS "-O3 -g -fma -align all -align array64byte" CACHE STRING "")
+SET(CMAKE_C_FLAGS "-O2 -g -m64 -ftree-vectorize -funroll-loops -mavx2 -march=core-avx2" CACHE STRING "")
+SET(CMAKE_CXX_FLAGS "-O2 -g -m64 -ftree-vectorize -funroll-loops -mavx2 -march=core-avx2" CACHE STRING "")
+SET(CMAKE_Fortran_FLAGS "-O2 -g -m64 -ftree-vectorize -funroll-loops -mavx2 -march=core-avx2" CACHE STRING "")
 
-# BLAS and LAPACK (from MKL), no threading
+# BLAS and LAPACK (from MKL), no threading as MPI_INIT is used
 SET(BLAS_LIBRARIES $ENV{MKLROOT}/lib/intel64/libmkl_scalapack_lp64.so
 		   $ENV{MKLROOT}/lib/intel64/libmkl_intel_lp64.so
 		   $ENV{MKLROOT}/lib/intel64/libmkl_core.so
@@ -48,5 +50,4 @@ SET(SCALAPACK_LIBRARIES $ENV{MKLROOT}/lib/intel64/libmkl_scalapack_lp64.so
 		   /usr/lib64/libm.so
 		   CACHE STRING "")
 
-ADD_DEFINITIONS(-I$ENV{MKLROOT}/include)
-
+ADD_DEFINITIONS(-m64 -I$ENV{MKLROOT}/include -Wl,--no-as-needed)
