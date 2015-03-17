ElmerCSC/elmerfem

commit 0ac3ca0f18fc1b8287cddf17f79579880b00af01

[FIX]ed a bug when restarting selected variables from a restart file, if one or more ignored variables is not defined on all nodes (SIZE(Var % Perm) /= SIZE(Var % Values)).

fem/src/ModelDescription.F90
@@ -3183,7 +3183,7 @@ CONTAINS
           IF( FieldSize == 0 ) THEN
             CALL Fatal('LoadRestartFile','Cannot pick variables with old format!')
           END IF
-          IF( Dofs == 1 ) RestartVariableSizes(DofCount) = FieldSize
+          IF( Dofs == 1 ) RestartVariableSizes(DofCount) = PermSize
         END IF
       END IF
 

commit 7f68cff758ef8e070b0c23a816e7a673a7dd4fd4

[FIX]ed a bug relating to restarting from previous simulation introduced by 0ac3ca0f18fc1b8287cddf17f79579880b00af01

fem/src/ModelDescription.F90
@@ -3183,7 +3183,13 @@ CONTAINS
           IF( FieldSize == 0 ) THEN
             CALL Fatal('LoadRestartFile','Cannot pick variables with old format!')
           END IF
-          IF( Dofs == 1 ) RestartVariableSizes(DofCount) = PermSize
+          IF( Dofs == 1 ) THEN
+             IF(PermSize > 0) THEN
+                RestartVariableSizes(DofCount) = PermSize
+             ELSE
+                RestartVariableSizes(DofCount) = FieldSize
+             END IF
+          END IF
         END IF
       END IF
 