c3be1a9b6d40c36bfca718949b6f520a025196e6
Juha Ruokolainen <jpr@ruokolainen.x.csc.fi>
2015-03-26 13:52:39 +0200

    Add test using isoparametric p-elements, geometry resolved by "Mesh Update" solver.

---
+++ fem/tests/CurvedBndryPFEM2/CMakeFiles/CurvedBndryPFEM2_Poisson.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_Fortran
+  "/home/jpr/wrk/ff/gitelmer/fem/tests/CurvedBndryPFEM2/Poisson.f90" "/home/jpr/wrk/ff/gitelmer/fem/tests/CurvedBndryPFEM2/CMakeFiles/CurvedBndryPFEM2_Poisson.dir/Poisson.f90.o"
+  )
+SET(CMAKE_Fortran_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "CONTIG=,CONTIGUOUS"
+  "HAVE_EXECUTECOMMANDLINE"
+  "USE_ARPACK"
+  "USE_ISO_C_BINDINGS"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jpr/wrk/ff/gitelmer/fem/src/CMakeFiles/elmersolver.dir/DependInfo.cmake"
+  "/home/jpr/wrk/ff/gitelmer/matc/src/CMakeFiles/matc.dir/DependInfo.cmake"
+  "/home/jpr/wrk/ff/gitelmer/umfpack/src/umfpack/CMakeFiles/umfpack.dir/DependInfo.cmake"
+  "/home/jpr/wrk/ff/gitelmer/umfpack/src/amd/CMakeFiles/amd.dir/DependInfo.cmake"
+  "/home/jpr/wrk/ff/gitelmer/fhutiter/src/CMakeFiles/fhuti.dir/DependInfo.cmake"
+  "/home/jpr/wrk/ff/gitelmer/fem/src/binio/CMakeFiles/binio.dir/DependInfo.cmake"
+  "/home/jpr/wrk/ff/gitelmer/mathlibs/src/arpack/CMakeFiles/arpack.dir/DependInfo.cmake"
+  "/home/jpr/wrk/ff/gitelmer/mathlibs/src/parpack/CMakeFiles/parpack.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+SET(CMAKE_Fortran_TARGET_MODULE_DIR "/home/jpr/wrk/ff/gitelmer/fmodules")
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/jpr/wrk/openmpi/include"
+  "/home/jpr/wrk/openmpi/lib"
+  "."
+  "fem/src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ fem/tests/CurvedBndryPFEM2/CMakeFiles/CurvedBndryPFEM2_Poisson.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/CurvedBndryPFEM2_Poisson.dir/Poisson.f90.o"
+  "Poisson.pdb"
+  "Poisson.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang Fortran)
+  INCLUDE(CMakeFiles/CurvedBndryPFEM2_Poisson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
