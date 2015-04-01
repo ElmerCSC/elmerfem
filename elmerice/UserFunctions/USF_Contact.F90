3195c2fbaaad81e6a5f0fe6538e02b2b7d5ba06f
J Todd <jat71@cam.ac.uk>
2015-04-01 17:50:31 +0300

    Added prescribed friction to USF_Contact sliding law options.

--- elmerice/UserFunctions/USF_Contact.F90
+++ elmerice/UserFunctions/USF_Contact.F90
@@ -47,16 +47,18 @@ FUNCTION SlidCoef_Contact ( Model, nodenumber, y) RESULT(Bdrag)
 
   TYPE(Model_t) :: Model
   TYPE(Solver_t) :: Solver
-  TYPE(variable_t), POINTER :: TimeVar, NormalVar, VarSurfResidual, GroundedMaskVar, HydroVar, DistanceVar
+  TYPE(variable_t), POINTER :: TimeVar, NormalVar, VarSurfResidual, GroundedMaskVar, HydroVar, DistanceVar, FrictionVar
   TYPE(ValueList_t), POINTER :: BC
   TYPE(Element_t), POINTER :: Element, CurElement, BoundaryElement
   TYPE(Nodes_t), SAVE :: Nodes
 
-  REAL(KIND=dp), POINTER :: NormalValues(:), ResidValues(:), GroundedMask(:), Hydro(:), Distance(:)
+  REAL(KIND=dp), POINTER :: NormalValues(:), ResidValues(:), GroundedMask(:), Hydro(:), &
+       Distance(:), FrictionValues(:)
   REAL(KIND=dp) :: Bdrag, t, told, thresh
   REAL(KIND=dp), ALLOCATABLE :: Normal(:), Fwater(:), Fbase(:)
 
-  INTEGER, POINTER :: NormalPerm(:), ResidPerm(:), GroundedMaskPerm(:), HydroPerm(:), DistancePerm(:)
+  INTEGER, POINTER :: NormalPerm(:), ResidPerm(:), GroundedMaskPerm(:), HydroPerm(:), &
+       DistancePerm(:), FrictionPerm(:)
   INTEGER :: nodenumber, ii, DIM, GL_retreat, n, tt, Nn, jj, MSum, ZSum
 
   LOGICAL :: FirstTime = .TRUE., GotIt, Yeschange, GLmoves, Friction
@@ -64,10 +66,11 @@ FUNCTION SlidCoef_Contact ( Model, nodenumber, y) RESULT(Bdrag)
   REAL (KIND=dp) ::  y, relChange, relChangeOld, Sliding_Budd, Sliding_Weertman, Friction_Coulomb
 
   REAL(KIND=dp) :: comp, cond, TestContact
-  CHARACTER(LEN=MAX_NAME_LEN) :: USF_Name='SlidCoef_Contact', Sl_Law, GLtype
+  CHARACTER(LEN=MAX_NAME_LEN) :: USF_Name='SlidCoef_Contact', Sl_Law, GLtype, FrictionVarName
 
   SAVE FirstTime, yeschange, told, GLmoves, thresh, GLtype, TestContact
   SAVE DIM, USF_Name, Normal, Fwater, Fbase, relChangeOld, Sl_Law
+  SAVE FrictionVar, FrictionValues, FrictionPerm, BC
 
 !----------------------------------------------------------------------------
 
@@ -110,7 +113,7 @@ FUNCTION SlidCoef_Contact ( Model, nodenumber, y) RESULT(Bdrag)
      IF (.NOT.Gotit) THEN
         CALL FATAL(USF_Name,'No "Sliding law" Name given')
      END IF
-     
+
      GLtype = GetString( BC, 'Grounding Line Definition', GotIt )
      IF (.NOT.Gotit) THEN
         GLtype = 'last grounded'
@@ -155,6 +158,21 @@ FUNCTION SlidCoef_Contact ( Model, nodenumber, y) RESULT(Bdrag)
         CALL INFO( USF_Name, 'far inland nodes will not detach', level=3)
      END IF
   ENDIF
+  
+  IF(Sl_Law(1:10) == 'prescribed') THEN
+     FrictionVarName = GetString( BC, 'Friction Variable Name', GotIt )
+     IF(.NOT. GotIt) CALL Fatal(USF_Name, 'Prescribed friction requested but no &
+          "Friction Variable Name" found!')
+     FrictionVar => VariableGet(Model % Mesh % Variables, FrictionVarName)
+     IF(ASSOCIATED(FrictionVar)) THEN
+        FrictionValues => FrictionVar % Values
+        FrictionPerm => FrictionVar % Perm
+     ELSE
+        WRITE(Message, '(A,A)') 'Unable to find variable: ',FrictionVarName
+        CALL Fatal(USF_Name, Message)
+     END IF
+  END IF
+
 
   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   ! First time step for a New time
@@ -357,6 +375,10 @@ FUNCTION SlidCoef_Contact ( Model, nodenumber, y) RESULT(Bdrag)
            Bdrag = Sliding_Budd(Model, nodenumber, y)
         CASE ('coulomb')
            Bdrag = Friction_Coulomb(Model, nodenumber, y)
+        CASE('prescribed beta2')
+           Bdrag = FrictionValues(FrictionPerm(nodenumber))**2.0_dp
+        CASE('prescribed power')
+           Bdrag = 10.0_dp**FrictionValues(FrictionPerm(nodenumber))
         CASE DEFAULT
            WRITE(Message, '(A,A)') 'Sliding law not recognised ',Sl_law
            CALL FATAL( USF_Name, Message)
