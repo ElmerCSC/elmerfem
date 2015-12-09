d8ea265cddf6753440813ae3d9e5cc6be8c6880b
eelis.takala <eelis.takala@trafotek.fi>
2015-12-09 15:59:57 +0200

    Changing nu_tensor imaginary in harmonic case.

--- fem/src/modules/MagnetoDynamics2D.F90
+++ fem/src/modules/MagnetoDynamics2D.F90
@@ -1061,7 +1061,8 @@ CONTAINS
     COMPLEX(KIND=dp) :: MASS(nd,nd), STIFF(nd,nd), FORCE(nd), LoadAtIp,&
       JAC(nd,nd),Agrad(3),Load(n),M(2,n),M_ip(2),POTC(nd), C(n), C_ip
 
-    REAL(KIND=dp) :: POT(2,nd),R(n),Babs,mu,muder,Omega,nu_tensor(2,2)
+    REAL(KIND=dp) :: POT(2,nd),R(n),Babs,mu,muder,Omega
+    COMPLEX(KIND=dp) :: nu_tensor(2,2)
 
     LOGICAL :: Cubic, HBcurve, Found, Stat
 
@@ -1075,7 +1076,8 @@ CONTAINS
     LOGICAL :: CoilBody    
     TYPE(ValueList_t), POINTER :: CompParams
 
-    REAL(KIND=dp) :: Bt(nd,2), Ht(nd,2) 
+    REAL(KIND=dp) :: Bt(nd,2)
+    COMPLEX(KIND=dp) :: Ht(nd,2) 
 
 !$omp threadprivate(Nodes)
 !------------------------------------------------------------------------------
@@ -1179,8 +1181,8 @@ CONTAINS
       IF ( CSymmetry ) Bt(:,2) = Bt(:,2) + Basis(:)/x
       
       nu_tensor = 0.0_dp
-      nu_tensor(1,1) = mu ! Mu is really nu!!! too lazy to correct now...
-      nu_tensor(2,2) = mu
+      nu_tensor(1,1) = CMPLX(mu, 0, KIND=dp) ! Mu is really nu!!! too lazy to correct now...
+      nu_tensor(2,2) = CMPLX(mu, 0, KIND=dp)
 
       DO p = 1,nd
         Ht(p,:) = MATMUL(nu_tensor, Bt(p,:))
