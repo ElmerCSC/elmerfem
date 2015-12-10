bd8a3e1b002e0f42110264a442dcd01f4ea01747
Peter Råback <raback@csc.fi>
2015-12-10 09:28:43 +0200

    fixed a typo

--- fem/src/ElmerSolver.F90
+++ fem/src/ElmerSolver.F90
@@ -711,7 +711,7 @@ END INTERFACE
            Tol = ListGetConstReal( Solver % Values,'Reference Norm Tolerance', Found )
            IF(.NOT. Found ) Tol = 1.0d-5
            Norm = Var % Norm 
-           IF( ListGetLogidal( Solver % Values,'Reference Norm Absolute', Found ) ) THEN
+           IF( ListGetLogical( Solver % Values,'Reference Norm Absolute', Found ) ) THEN
              Err = ABS( Norm - RefNorm ) 
            ELSE
              Err = ABS( Norm - RefNorm ) / RefNorm 
