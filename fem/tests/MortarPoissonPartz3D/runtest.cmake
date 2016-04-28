2cd8b87a26f914fe1d9198611176c2f16ea27e50
Peter Råback <peter.raback@csc.fi>
2016-04-28 14:57:05 +0300

    test case for nonconformal mortar projector when the mortar interface is given to several partitions requiring halo elements

---
+++ fem/tests/MortarPoissonPartz3D/runtest.cmake
@@ -0,0 +1,4 @@
+execute_process(COMMAND ${ELMERGRID_BIN} 14 2 mesh.msh -autoclean)
+execute_process(COMMAND ${ELMERGRID_BIN} 2 2 mesh -partdual -metis ${MPIEXEC_NTASKS} 3 -connect 4 10 -partconnect 3 -haloz -nooverwrite)
+
+RUN_ELMER_TEST()
