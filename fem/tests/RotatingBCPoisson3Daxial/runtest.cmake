20be2b16f92db07055554bcd8cd1580a3220d820
Peter Raback <peter.raback@csc.fi>
2016-12-20 12:15:58 +0200

    minor fix to test case

--- fem/tests/RotatingBCPoisson3Daxial/runtest.cmake
+++ fem/tests/RotatingBCPoisson3Daxial/runtest.cmake
@@ -1,4 +1,4 @@
 execute_process(COMMAND ${ELMERGRID_BIN} 14 2 sector.msh -autoclean)
-execute_process(COMMAND ${ELMERGRID_BIN} extrude_sector.eg)
+execute_process(COMMAND ${ELMERGRID_BIN} sector_extrude.eg)
 
 RUN_ELMER_TEST()
