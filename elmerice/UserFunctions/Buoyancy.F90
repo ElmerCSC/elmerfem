456fc8d52f62ef9e30ab04ca24adeaee0fb6a92e
Cheng Gong <cheng.gong@it.uu.se>
2017-02-15 11:11:23 +0100

    add function to compute the water pressure at bedrock

--- elmerice/UserFunctions/Buoyancy.F90
+++ elmerice/UserFunctions/Buoyancy.F90
@@ -501,3 +501,273 @@ FUNCTION SeaSpring ( Model, nodenumber, y) RESULT(C)
       C = 1.0e20_dp
    END IF
 END FUNCTION SeaSpring
+
+
+
+FUNCTION BedPressure ( Model, nodenumber, y) RESULT(pw)
+   USE types
+   USE CoordinateSystems
+   USE SolverUtils
+   USE ElementDescription
+   USE DefUtils
+   IMPLICIT NONE
+   TYPE(Model_t) :: Model
+   TYPE(Solver_t):: Solver
+   TYPE(Nodes_t), SAVE :: Nodes
+   TYPE(variable_t), POINTER :: Timevar
+   TYPE(Element_t), POINTER ::  BoundaryElement, BCElement, CurElement, ParentElement
+   TYPE(ValueList_t), POINTER :: BC, material, ParentMaterial, BodyForce
+   INTEGER :: NBoundary, NParent, BoundaryElementNode, ParentElementNode, body_id, other_body_id, material_id
+   INTEGER :: nodenumber, NumberOfNodesOnBoundary 
+   INTEGER, ALLOCATABLE :: NodeOnBoundary(:)
+   INTEGER :: Nn, i, j, p, n, Nmax, bf_id, DIM, bf_id_FS 
+   REAL(KIND=dp) :: y, pw, t, told, dt, Bu, Bv
+   REAL(KIND=dp) :: Zsl, rhow, gravity
+   REAL(KIND=dp), ALLOCATABLE :: S(:), auxReal(:), Ns(:),  a_perp(:), SourceFunc(:), normal(:,:)
+   LOGICAL :: FirstTime = .TRUE., NewTime, GotIt, ComputeS,  NormalFlux = .TRUE. 
+   CHARACTER(LEN=MAX_NAME_LEN)  :: BottomSurfaceName
+       
+!------------------------------------------------------------
+  TYPE(ValueList_t), POINTER :: SolverParams
+  TYPE(Variable_t), POINTER :: bedrockVar
+  LOGICAL :: AllocationsDone = .FALSE.
+
+  INTEGER :: bedrockSource
+  INTEGER, POINTER ::  bedrockPerm(:)
+  CHARACTER(LEN=MAX_NAME_LEN) ::  bedrockName
+  INTEGER,PARAMETER :: MATERIAL_DEFAULT = 1, MATERIAL_NAMED = 2, VARIABLE = 3
+
+  SAVE AllocationsDone
+
+!------------------------------------------------------------
+
+
+   SAVE told, FirstTime, NewTime, Nn, dt, Ns, Bodyforce, DIM
+   SAVE S, rhow, gravity, Zsl, auxReal, NormalFlux, a_perp, SourceFunc
+   SAVE NumberOfNodesOnBoundary, NodeOnBoundary, normal
+   SAVE BottomSurfaceName, bf_id_FS 
+   
+  !--------------------------------------------------------------
+  ! Allocate some permanent storage:
+  !--------------------------------------------------------------
+   Timevar => VariableGet( Model % Variables,'Time')
+   t = TimeVar % Values(1)
+   dt = Model % Solver % dt 
+
+   IF (FirstTime) THEN
+      FirstTime = .FALSE.
+      NewTime = .TRUE.
+      told = t
+      ALLOCATE( NodeOnBoundary( Model % Mesh % NumberOfNodes ))
+      n = Model % MaxElementNodes 
+      ALLOCATE( auxReal(n), SourceFunc(n) )
+      DIM = CoordinateSystemDimension()
+
+      rhow = GetConstReal( Model % Constants, 'Water Density', GotIt )
+      IF (.NOT.GotIt) THEN
+         WRITE(Message,'(A)') 'Variable Water Density not found. &
+              &Setting to 1.03225e-18'
+         CALL INFO('BedPressure', Message, level=20)
+         rhow = 1.03225e-18_dp
+      ELSE
+         WRITE(Message,'(A,F10.4)') 'Water Density = ', rhow
+         CALL INFO('BedPressure', Message , level = 20)
+      END IF
+      
+!-----------------------------------------------------------------
+! Is there basal melt to account for          
+!-----------------------------------------------------------------
+      NormalFlux = GetLogical( Model % Constants, 'Buoyancy Use Basal Melt', GotIt )    
+      IF (.NOT.GotIt) THEN
+         WRITE(Message,'(A)') 'Variable Buoyancy Use Basal Melt not found. &
+              &Setting to FALSE'
+         CALL INFO('BedPressure', Message, level=20)
+         NormalFlux = .FALSE.
+      ELSE
+         WRITE(Message,*) 'Buoyancy Use Basal Melt = ', NormalFlux
+         CALL INFO('BedPressure', Message , level = 20)
+      END IF
+
+!-----------------------------------------------------------------
+! Find the bf_id of the FS solvers if NormalFlux is TRUE
+! and read the name of the Bottom Free surface solver
+!-----------------------------------------------------------------
+      IF (NormalFlux) THEN
+        BottomSurfaceName = GetString( Model % Constants, 'Bottom Surface Name', GotIt )    
+        IF (.NOT.GotIt) THEN
+           WRITE(Message,'(A)') 'Variable Bottom SUrface Name not found. &
+                &Setting to DyBottom'
+           CALL INFO('BedPressure', Message, level=20)
+           BottomSurfaceName = 'DyBottom'
+        ELSE
+           WRITE(Message,*)'Bottom Surface Name = ',TRIM(BottomSurfaceName)
+           CALL INFO('BedPressure', Message , level = 20)
+        END IF
+
+        bf_id_FS = -1
+        DO bf_id=1,Model % NumberOFBodyForces
+           IF( ListCheckPresent( Model % BodyForces(bf_id) % Values, &
+                TRIM(BottomSurfaceName)//' Accumulation') ) bf_id_FS = bf_id 
+        END DO
+
+        IF (bf_id_FS<0) THEN 
+           CALL FATAL('BedPressure','No Basal Melt found in any body Force')
+        END IF 
+      END IF
+
+!-----------------------------------------------------------------
+! Read the gravity 
+!-----------------------------------------------------------------
+      bf_id = ListGetInteger( Model % Bodies(1) % Values,&
+           'Body Force',  minv=1, maxv=Model % NumberOFMaterials)
+
+      IF (DIM==2) THEN    !NOT the same keyword if the shape factor is used
+        gravity = -GetConstReal( Model % BodyForces(bf_id) % Values, 'Lateral Friction Gravity 2',Gotit)
+        IF (.NOT. Gotit) THEN
+          gravity = -GetConstReal( Model % BodyForces(bf_id) % Values, 'Flow BodyForce 2',Gotit)
+        END IF
+      ELSE
+        gravity = -GetConstReal( Model % BodyForces(bf_id) % Values, 'Flow BodyForce 3',Gotit)
+      END IF
+ 
+!-----------------------------------------------------------------
+! Number of nodes concerned (needed to compute Ns from the normal)
+!-----------------------------------------------------------------
+
+      CurElement => Model % CurrentElement
+      NodeOnBoundary = 0
+      NumberOfNodesOnBoundary = 0
+      DO p = 1, Model % NumberOfBoundaryElements
+         BCElement => GetBoundaryElement(p) 
+         BC => GetBC( BCElement ) 
+         IF( GetElementFamily(BCElement) == 1 ) CYCLE
+         ComputeS = GetLogical( BC, 'Compute Bed Pressure', GotIt)
+         IF (.Not.GotIt) ComputeS = .FALSE.
+         IF (ComputeS) THEN
+            n = BCElement % Type % NumberOfNodes
+            DO i = 1, n
+               j = BCElement % NodeIndexes (i)
+               IF (NodeOnBoundary(j)==0) THEN
+                 NumberOfNodesOnBoundary = NumberOfNodesOnBoundary + 1
+                 NodeOnBoundary(j) = NumberOfNodesOnBoundary
+               END IF
+            END DO
+         END IF
+      END DO
+      Model % CurrentElement => CurElement 
+
+      ALLOCATE( S( NumberOfNodesOnBoundary ), Ns( NumberOfNodesOnBoundary ))
+      IF (NormalFlux) ALLOCATE( a_perp ( NumberOfNodesOnBoundary ))
+      
+   ELSE
+      IF (t > told) THEN
+         NewTime = .TRUE.
+         told = t
+      END IF
+   ENDIF  ! FirstTime
+   
+   IF (NewTime) THEN
+      NewTime = .FALSE.
+
+!-----------------------------------------------------------------
+! get some information upon active boundary element and its parent
+!-----------------------------------------------------------------
+      BoundaryElement => Model % CurrentElement
+      IF ( .NOT. ASSOCIATED(BoundaryElement) ) THEN
+         CALL FATAL('BedPressure','No boundary element found')
+      END IF
+      other_body_id = BoundaryElement % BoundaryInfo % outbody
+      IF (other_body_id < 1) THEN ! only one body in calculation
+         ParentElement => BoundaryElement % BoundaryInfo % Right
+         IF ( .NOT. ASSOCIATED(ParentElement) ) ParentElement => BoundaryElement % BoundaryInfo % Left
+      ELSE ! we are dealing with a body-body boundary and asume that the normal is pointing outwards
+         ParentElement => BoundaryElement % BoundaryInfo % Right
+         IF (ParentElement % BodyId == other_body_id) ParentElement => BoundaryElement % BoundaryInfo % Left
+      END IF
+      
+      body_id = ParentElement % BodyId
+      material_id = ListGetInteger(Model % Bodies(body_id) % Values, 'Material', GotIt)
+      ParentMaterial => Model % Materials(material_id) % Values
+      IF ((.NOT. ASSOCIATED(ParentMaterial)) .OR. (.NOT. GotIt)) THEN
+         WRITE(Message,'(A,I10,A,I10)')&
+              'No material values found for body no ', body_id,&
+              ' under material id ', material_id
+         CALL FATAL('BedPressure',Message)
+      END IF
+      NParent = ParentElement % Type % NumberOfNodes
+      
+!-------------------------
+! Get material parameters
+! Sea level for that time
+!-------------------------
+     
+      Zsl = GetConstReal( ParentMaterial, 'Sea level', GotIt )
+
+      IF (.NOT.GotIt) THEN
+         WRITE(Message,'(A)') 'Variable Sea level not found. &
+              &Setting to 0.0'
+         CALL INFO('BedPressure', Message, level=2)
+         Zsl = 0.0_dp
+      ELSE
+         WRITE(Message,'(A,F10.4)') 'Sea level = ', Zsl 
+         CALL INFO('BedPressure', Message , level = 2)
+      END IF
+           
+
+!-------------------------
+!  Get bedrock topography
+!-------------------------
+      Solver = Model % Solver
+      IF ( (.NOT. AllocationsDone)) THEN
+         CALL INFO( 'BedPressure', 'Memory allocation done.',Level=1 )
+         AllocationsDone = .TRUE.
+         SolverParams => GetSolverParams()
+        IF (.NOT.GotIt) THEN
+           CALL FATAL('BedPressure', 'No tolerance given for the Grounded Mask.')
+        END IF
+
+        bedrockName = GetString(SolverParams, 'Bedrock Variable', GotIt)
+        IF (GotIt) THEN
+           bedrockSource = VARIABLE
+           CALL info('BedPressure', 'Bedrock Variable name found', level=8)
+        ELSE
+           bedrockName = GetString(SolverParams, 'Bedrock Material', GotIt)
+           IF (GotIt) THEN
+              bedrockSource = MATERIAL_NAMED
+              CALL info('BedPressure', 'Bedrock Material name found', level=8)
+           ELSE
+              bedrockSource = MATERIAL_DEFAULT     
+              CALL info('BedPressure', 'No Bedrock Variable or Material; searching for material \"Min Zs Bottom\".', level=8)
+           END IF
+        END IF
+          CurElement => Model % CurrentElement
+
+         SELECT CASE(bedrockSource)
+         CASE (VARIABLE)
+            bedrockVar => VariableGet(Model % Mesh % Variables, bedrockName )
+            IF (.NOT. ASSOCIATED(bedrockVar)) CALL FATAL('BedPressure',"Could not find bedrock variable")
+            bedrockPerm => bedrockVar % Perm
+            DO j = 1, NumberOfNodesOnBoundary
+              S(j) =  bedrockVar % values(bedrockPerm(j))
+            END DO
+            NULLIFY(bedrockPerm)
+            NULLIFY(bedrockVar)
+         CASE (MATERIAL_NAMED)
+!             Material => GetMaterial( BCElement )
+!             S = ListGetReal( Material,bedrockName, NumberOfNodesOnBoundary , & 
+!                  BCElement % NodeIndexes, GotIt )
+!             IF (.NOT. GotIt) CALL FATAL('BedPressure',"Could not find bedrock material")
+         CASE (MATERIAL_DEFAULT)
+!             Material => GetMaterial( BCElement )
+!             S = ListGetReal( Material,'Min Zs Bottom',NumberOfNodesOnBoundary , & 
+!                  BCElement % NodeIndexes, GotIt )
+!             IF (.NOT. GotIt) CALL FATAL('BedPressure',"Could not find bedrock material")
+         END SELECT
+
+      END IF  
+   ENDIF  ! new dt
+    
+   j = NodeOnBoundary( nodenumber )
+   pw = -gravity * rhow * (Zsl - S(j))  
+   IF (pw > 0.0) pw = 0.0
+END FUNCTION BedPressure
