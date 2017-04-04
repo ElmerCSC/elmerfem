cfe70400da7214071ff6868818415a6f56ae3eb3
J Todd <jat39@st-andrews.ac.uk>
2017-04-04 17:41:01 +0100

    Finishing 3D calving test case.
    Added an additional distance solver after the simulation to provide a testable norm.

--- elmerice/Tests/Calving3D/runTest.cmake
+++ elmerice/Tests/Calving3D/runTest.cmake
@@ -1,5 +1,7 @@
 INCLUDE(${TEST_SOURCE}/../test_macros.cmake)
 
-EXECUTE_PROCESS(COMMAND ${ELMERGRID_BIN} 14 2 PlanMesh.msh -autoclean -metis 2 0)
+SET(NPROCS 2)
 
-RUN_ELMERICE_TEST()
+EXECUTE_PROCESS(COMMAND ${ELMERGRID_BIN} 14 2 PlanMesh.msh -autoclean -metis ${NPROCS} 0)
+
+RUN_ELMERICE_TEST(WITH_MPI)
