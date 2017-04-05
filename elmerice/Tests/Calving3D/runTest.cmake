8ea517d62abd9cebc289306f8ee23a8648f5158c
J Todd <jat39@st-andrews.ac.uk>
2017-04-05 14:51:05 +0100

    Adding necessary COPY statements for duplicate dynamic solvers

--- elmerice/Tests/Calving3D/runTest.cmake
+++ elmerice/Tests/Calving3D/runTest.cmake
@@ -4,4 +4,22 @@ SET(NPROCS 2)
 
 EXECUTE_PROCESS(COMMAND ${ELMERGRID_BIN} 14 2 PlanMesh.msh -autoclean -metis ${NPROCS} 0)
 
+FILE(COPY ${BINARY_DIR}/fem/src/modules/FreeSurfaceSolver${SHLEXT} DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/")
+FILE(RENAME FreeSurfaceSolver${SHLEXT} FreeSurfaceSolver1${SHLEXT})
+
+FILE(COPY ${BINARY_DIR}/fem/src/modules/MeshSolve${SHLEXT} DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/")
+FILE(RENAME MeshSolve${SHLEXT} MeshSolve1${SHLEXT})
+
+FILE(COPY ${BINARY_DIR}/fem/src/modules/MeshSolve${SHLEXT} DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/")
+FILE(RENAME MeshSolve${SHLEXT} MeshSolve2${SHLEXT})
+
+FILE(COPY ${BINARY_DIR}/fem/src/modules/ResultOutputSolve${SHLEXT} DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/")
+FILE(RENAME ResultOutputSolve${SHLEXT} ResultOutputSolve1${SHLEXT})
+
+FILE(COPY ${BINARY_DIR}/fem/src/modules/ResultOutputSolve${SHLEXT} DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/")
+FILE(RENAME ResultOutputSolve${SHLEXT} ResultOutputSolve2${SHLEXT})
+
+FILE(COPY ${BINARY_DIR}/fem/src/modules/ResultOutputSolve${SHLEXT} DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/")
+FILE(RENAME ResultOutputSolve${SHLEXT} ResultOutputSolve3${SHLEXT})
+
 RUN_ELMERICE_TEST(WITH_MPI)
