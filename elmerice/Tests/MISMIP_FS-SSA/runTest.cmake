600c94202553e487fb99ffe1513d4745e7cf1df0
tzwinger <zwingerthomas@gmail.com>
2017-07-17 15:40:53 +0300

    small corrections

--- elmerice/Tests/MISMIP_FS-SSA/runTest.cmake
+++ elmerice/Tests/MISMIP_FS-SSA/runTest.cmake
@@ -1,7 +1,3 @@
 INCLUDE(${TEST_SOURCE}/../test_macros.cmake)
 
-FILE(COPY ${BINARY_DIR}/fem/src/modules/FreeSurfaceSolver${SHLEXT} DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/")
-
-FILE(RENAME FreeSurfaceSolver${SHLEXT}  MyFreeSurfaceSolver)
-
 RUN_ELMERICE_TEST()
