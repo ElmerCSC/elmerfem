ElmerCSC/elmerfem

commit e9fd3936771fd5aea79657fdc6e3110dc8f4fc74

Collection of multithreading improvements. Also some minor fixes regarding code correctness.
Multithreading improvements:

- Added multithreading to HUTI double precision CG algorithm.

- Added partial implementation of ListMatrixArray type and
  module. ListMatrixArray which offers the same operations as ListMatrix but
  allows thread safe access per matrix row. ListMatrixArray also pools
  ListEntry allocations per thread, which is more time efficient with multiple
  threads than allocating ListEntries one at a time (less locking in the
  Fortran runtime and OS).

- Modified CreateMatrix to use ListMatrixArray and multicoloring when
  constructing the initial matrix sparsity structure and multithreaded startup
  is enabled.

- Added partial multithreading to selected matrix scaling routines.

- Added multithreading to matrix bandwidth computation. Bandwidth optimization
  routines not multithreaded due to serial nature of the used RCM algorithm.

Bug fixes:

- Corrected race conditions arising from implicit SAVE variables from
  MagnetoDynamicsUtils and MagnetoDynamics.

- Corrected OpenFOAM IO module to work with the Intel Fortran compiler.

- Corrected SaveLine output level to use the maximum of 31

- Corrected uninitialized variable access in ElmerGrid

Misc. improvements:

- Boundary mesh is now also colored in AddEquationBasics when MultiColour
  Solver is set to True. Colour index lists for boundary mesh are available in
  Mesh % BoundaryColourIndexList similarly to regular colour index lists.

- Modified MakePermUsingMask to use List_AllocateMatrix

fem/src/MagnetoDynamicsUtils.F90
@@ -10,7 +10,7 @@
                    CoilBody,CoilType) RESULT (Tcoef)  
 !------------------------------------------------------------------------------
     IMPLICIT NONE
-    REAL(KIND=dp), POINTER :: Cwrk(:,:,:) => Null()
+    REAL(KIND=dp), POINTER :: Cwrk(:,:,:)
     TYPE(Element_t), POINTER :: Element
     INTEGER :: n, i, j
     TYPE(Valuelist_t), POINTER :: Material
@@ -20,6 +20,7 @@
     LOGICAL :: Found
     LOGICAL :: CoilBody
 
+    Cwrk => NULL()
     Tcoef=0._dp
     Material => GetMaterial( Element )
     IF ( ASSOCIATED(Material) ) THEN
@@ -95,7 +96,7 @@
                   RESULT (mu)
 !------------------------------------------------------------------------------
     IMPLICIT NONE
-    REAL(KIND=dp), POINTER :: Cwrk(:,:,:) => Null()
+    REAL(KIND=dp), POINTER :: Cwrk(:,:,:)
     TYPE(Element_t), POINTER :: Element
     INTEGER :: n, i, j
     TYPE(Valuelist_t), POINTER :: Material
@@ -103,6 +104,7 @@
     CHARACTER(LEN=2) :: Part
     LOGICAL :: Found
 
+    Cwrk => NULL()
     mu=0._dp
     Material => GetMaterial( Element )
     IF ( ASSOCIATED(Material) ) THEN
@@ -140,7 +142,7 @@
                   RESULT (T)
 !------------------------------------------------------------------------------
     IMPLICIT NONE
-    REAL(KIND=dp), POINTER :: Cwrk(:,:,:) => Null()
+    REAL(KIND=dp), POINTER :: Cwrk(:,:,:)
     TYPE(Element_t), POINTER :: Element
     INTEGER :: n, i, j, slen, tsize 
     TYPE(Valuelist_t), POINTER :: Material
@@ -149,6 +151,7 @@
     CHARACTER(LEN=*) :: varname
     LOGICAL, OPTIONAL :: Found
 
+    Cwrk => NULL()
     IF (.NOT. ASSOCIATED(Element)) CALL Fatal ('GetTensor', 'Element not associated')
     T=0._dp
     Material => GetMaterial( Element )

fem/src/modules/MagnetoDynamics2D.F90
@@ -433,7 +433,7 @@ CONTAINS
     LOGICAL :: Cubic, HBcurve, Found, Stat
 
     REAL(KIND=dp), POINTER :: Bval(:), Hval(:), Cval(:), &
-      CubicCoeff(:) => NULL(), HB(:,:) => NULL()
+      CubicCoeff(:), HB(:,:)
     TYPE(ValueListEntry_t), POINTER :: Lst
     TYPE(ValueList_t), POINTER :: Material, BodyForce
 
@@ -447,7 +447,8 @@ CONTAINS
     REAL(KIND=dp) :: nu_tensor(2,2)
     REAL(KIND=dp) :: B_ip(2), Alocal
 !------------------------------------------------------------------------------
-
+    CubicCoeff => NULL()
+    HB => NULL()
     CALL GetElementNodes( Nodes,Element )
     STIFF = 0._dp
     JAC  = 0._dp

elmergrid/src/femelmer.c
@@ -5116,7 +5116,8 @@ int SaveElmerInputPartitioned(struct FemType *data,struct BoundaryType *bound,
   int *bcnodesaved[MAXBCS],maxbcnodesaved,*bcelemsaved,*bcnode;
   int *bcnodedummy,*elementhalo,*neededtimes2;
   int partstart,partfin,filesetsize,nofile,nofile2,nobcnodes;
-  int halobulkelems,halobcs,savethis,fail,cdstat;
+  int halobulkelems,halobcs,savethis,fail=0,cdstat;
+
   FILE *out,*outfiles[MAXPARTITIONS+1];
   int sumelementsinpart,sumownnodes,sumsharednodes,sumsidesinpart,sumindirect;
 
@@ -5209,7 +5210,7 @@ int SaveElmerInputPartitioned(struct FemType *data,struct BoundaryType *bound,
 #ifdef MINGW32
   mkdir(directoryname);
 #else
-  mkdir(directoryname,0700);
+  fail = mkdir(directoryname,0700);
 #endif
   if(info && !fail) printf("Created mesh directory: %s\n",directoryname);
   fail = chdir(directoryname);

fem/src/BandwidthOptimize.F90
@@ -80,6 +80,11 @@ CONTAINS
      TYPE(ListMatrixEntry_t), POINTER :: CList
 !-------------------------------------------------------------------------------
      HalfBandWidth = 0
+     !$OMP PARALLEL DO &
+     !$OMP SHARED(List, Reorder, InvInitialReorder, N) & 
+     !$OMP PRIVATE(Clist, j, k) & 
+     !$OMP REDUCTION(max:HalfBandWidth) &
+     !$OMP DEFAULT(NONE)
      DO i=1,n
         CList => List(i) % Head
         j = i
@@ -95,6 +100,7 @@ CONTAINS
            Clist => Clist % Next
         END DO
      END DO
+     !$OMP END PARALLEL DO
 !-------------------------------------------------------------------------------
    END FUNCTION ComputeBandwidth
 !-------------------------------------------------------------------------------

fem/src/CMakeLists.txt
@@ -20,7 +20,7 @@ SET(solverlib_SOURCES AddrFunc.F90 NavierStokes.F90 NavierStokesGeneral.F90
   NavierStokesCylindrical.F90 Lists.F90
   DiffuseConvectiveAnisotropic.F90 LoadMod.F90
   DiffuseConvectiveGeneralAnisotropic.F90 PElementMaps.F90
-  PElementBase.F90 ElementDescription.F90 Integration.F90
+  PElementBase.F90 ElementDescription.F90 Integration.F90 ListMatrixArray.F90
   ModelDescription.F90 GeneralUtils.F90 Stress.F90 StressGeneral.F90
   LinearAlgebra.F90 CoordinateSystems.F90 ListMatrix.F90 CRSMatrix.F90
   BandMatrix.F90 BandwidthOptimize.F90 BlockSolve.F90

fem/src/DefUtils.F90
@@ -5870,6 +5870,118 @@ CONTAINS
     CALL Info('GetNOFColours','Number of colours: '//TRIM(I2S(ncolours)),Level=12)
   END FUNCTION GetNOFColours
 
+  
+  ! Check given colourings are valid and see if they are free of race conditions. 
+  SUBROUTINE CheckColourings(Solver)
+    IMPLICIT NONE
+    TYPE(Solver_t) :: Solver
+    
+    TYPE(Mesh_t), POINTER :: Mesh
+    TYPE(Graph_t), POINTER :: Colours
+    TYPE(Graph_t), POINTER :: BoundaryColours
+
+    TYPE(Element_t), POINTER :: Element
+    
+    INTEGER, ALLOCATABLE :: Indexes(:), DOFIndexes(:)
+    INTEGER :: col, elem, belem, NDOF, dof
+    LOGICAL :: errors
+
+    errors = .FALSE.
+    
+    Mesh => Solver % Mesh
+    Colours => Solver % ColourIndexList
+    BoundaryColours => Solver % BoundaryColourIndexList
+    
+    ! Allocate workspace and initialize it
+    ALLOCATE(Indexes(MAX(Mesh % NumberOfNodes,&
+          Mesh % NumberOfBulkElements*Mesh % MaxElementDOFs,&
+          Mesh%NumberOfBoundaryElements*Mesh % MaxElementDOFs)), &
+          DOFIndexes(Mesh % MaxElementDOFs))
+    Indexes = 0
+
+    ! Check that every element has a colour
+    IF (ASSOCIATED(Colours)) THEN
+       DO col=1,Colours % N
+          DO elem=Colours % Ptr(col), Colours%Ptr(col+1)-1
+             Indexes(Colours % Ind(elem))=Indexes(Colours % Ind(elem))+1
+          END DO
+       END DO
+       DO elem=1,Mesh % NumberOfBulkElements
+          IF (Indexes(elem) < 1 .OR. Indexes(elem) > 1) THEN
+             CALL Warn('CheckColourings','Element not colored correctly: '//i2s(elem))
+             errors = .TRUE.
+          END IF
+       END DO
+       
+       Indexes = 0
+       ! Check that colouring is free of race conditions
+       DO col=1,Colours % N
+          DO elem=Colours % Ptr(col), Colours%Ptr(col+1)-1
+             Element => Mesh % Elements(Colours % Ind(elem))
+             NDOF = GetElementDOFs( DOFIndexes, Element, Solver )
+             DO dof=1,NDOF
+                Indexes(DOFIndexes(dof))=Indexes(DOFIndexes(dof))+1
+             END DO
+          END DO
+          ! Check colouring
+          DO dof=1,Mesh % NumberOfBulkElements*Mesh % MaxElementDOFs
+             IF (Indexes(dof)>1) THEN
+                CALL Warn('CheckColourings','DOF not colored correctly: '//i2s(dof))
+                errors = .TRUE.
+             END IF
+             Indexes(dof)=0
+          END DO
+       END DO
+    END IF
+
+    ! Check that every boundary element has a colour
+    IF (ASSOCIATED(BoundaryColours)) THEN
+       
+       DO col=1,BoundaryColours % N
+          DO elem=BoundaryColours % Ptr(col), BoundaryColours%Ptr(col+1)-1
+             Indexes(BoundaryColours % Ind(elem))=Indexes(BoundaryColours % Ind(elem))+1
+          END DO
+       END DO
+       DO elem=Mesh % NumberOfBulkElements+1,Mesh % NumberOfBulkElements + Mesh % NumberOfBoundaryElements
+          belem = elem - Mesh % NumberOfBulkElements
+          IF (Indexes(belem) < 1 .OR. Indexes(belem) > 1) THEN
+             CALL Warn('CheckColourings','Boundary element not colored correctly: '//i2s(belem))
+             errors = .TRUE.
+          END IF
+       END DO
+       
+       Indexes = 0
+       ! Check that colouring is free of race conditions
+       DO col=1,BoundaryColours % N
+          DO elem=BoundaryColours % Ptr(col), BoundaryColours%Ptr(col+1)-1
+             Element => Mesh % Elements(Mesh % NumberOfBulkElements + BoundaryColours % Ind(elem))
+             NDOF = GetElementDOFs( DOFIndexes, Element, Solver, NotDG=.TRUE. )
+             ! WRITE (*,'(2(A,I0))') 'BELEM=', elem, ', CMAP=', BoundaryColours % Ind(elem)
+             ! WRITE (*,*) DOFIndexes(1:NDOF)
+             DO dof=1,NDOF
+                Indexes(DOFIndexes(dof))=Indexes(DOFIndexes(dof))+1
+                ! WRITE (*,'(4(A,I0))') 'EID=', Element % ElementIndex,', dof=', dof, &
+                !      ', ind=', DOFIndexes(dof), ', colour=', col
+             END DO
+          END DO
+          ! Check colouring
+          DO dof=1,Mesh % NumberOfBulkElements*Mesh % MaxElementDOFs
+             IF (Indexes(dof)>1) THEN
+                CALL Warn('CheckColourings','Boundary DOF not colored correctly: '//i2s(dof))
+                errors = .TRUE.
+             END IF
+             Indexes(dof)=0
+          END DO
+       END DO
+    END IF
+
+    IF (errors) THEN
+      CALL Warn('CheckColourings','Mesh colouring contained errors')
+    END IF
+    
+    DEALLOCATE(Indexes, DOFIndexes)
+  END SUBROUTINE CheckColourings
+
 
 END MODULE DefUtils
 

fem/src/ElmerSolver.F90
@@ -1470,6 +1470,7 @@ END INTERFACE
 !------------------------------------------------------------------------------
    SUBROUTINE ExecSimulation(TimeIntervals,  CoupledMinIter, &
               CoupledMaxIter, OutputIntervals, Transient, Scanning)
+     IMPLICIT NONE
       INTEGER :: TimeIntervals,CoupledMinIter, CoupledMaxIter,OutputIntervals(:)
       LOGICAL :: Transient,Scanning
 !------------------------------------------------------------------------------
@@ -1500,9 +1501,9 @@ END INTERFACE
      REAL(KIND=dp) :: RealTime
 #endif
      
-!$omp parallel
-!$   IF(.NOT.GaussPointsInitialized()) CALL GaussPointsInit
-!$omp end parallel
+     !$OMP PARALLEL
+     IF(.NOT.GaussPointsInitialized()) CALL GaussPointsInit()
+     !$OMP END PARALLEL
 
      nSolvers = CurrentModel % NumberOfSolvers
      DO i=1,nSolvers

commit 0aad3dba419dd4f44dbbcb1f9c26fc6d8a657c50

Correct memory leaks introduced in commit e9fd3936771fd5aea79657fdc6e3110dc8f4fc74.

fem/src/MagnetoDynamicsUtils.F90
@@ -10,7 +10,7 @@
                    CoilBody,CoilType) RESULT (Tcoef)  
 !------------------------------------------------------------------------------
     IMPLICIT NONE
-    REAL(KIND=dp), POINTER :: Cwrk(:,:,:)
+    REAL(KIND=dp), SAVE, POINTER :: Cwrk(:,:,:) => NULL()
     TYPE(Element_t), POINTER :: Element
     INTEGER :: n, i, j
     TYPE(Valuelist_t), POINTER :: Material
@@ -19,8 +19,8 @@
     CHARACTER(LEN=2) :: Part
     LOGICAL :: Found
     LOGICAL :: CoilBody
+!$OMP THREADPRIVATE(Cwrk)
 
-    Cwrk => NULL()
     Tcoef=0._dp
     Material => GetMaterial( Element )
     IF ( ASSOCIATED(Material) ) THEN
@@ -96,15 +96,15 @@
                   RESULT (mu)
 !------------------------------------------------------------------------------
     IMPLICIT NONE
-    REAL(KIND=dp), POINTER :: Cwrk(:,:,:)
+    REAL(KIND=dp), SAVE, POINTER :: Cwrk(:,:,:) => NULL()
     TYPE(Element_t), POINTER :: Element
     INTEGER :: n, i, j
     TYPE(Valuelist_t), POINTER :: Material
     REAL(KIND=dp) :: mu(3,3,n)
     CHARACTER(LEN=2) :: Part
     LOGICAL :: Found
+!$OMP THREADPRIVATE(Cwrk)
 
-    Cwrk => NULL()
     mu=0._dp
     Material => GetMaterial( Element )
     IF ( ASSOCIATED(Material) ) THEN
@@ -142,7 +142,7 @@
                   RESULT (T)
 !------------------------------------------------------------------------------
     IMPLICIT NONE
-    REAL(KIND=dp), POINTER :: Cwrk(:,:,:)
+    REAL(KIND=dp), POINTER :: Cwrk(:,:,:) => NULL()
     TYPE(Element_t), POINTER :: Element
     INTEGER :: n, i, j, slen, tsize 
     TYPE(Valuelist_t), POINTER :: Material
@@ -150,8 +150,8 @@
     CHARACTER(LEN=2) :: Part
     CHARACTER(LEN=*) :: varname
     LOGICAL, OPTIONAL :: Found
+!$OMP THREADPRIVATE(Cwrk)
 
-    Cwrk => NULL()
     IF (.NOT. ASSOCIATED(Element)) CALL Fatal ('GetTensor', 'Element not associated')
     T=0._dp
     Material => GetMaterial( Element )

fem/src/modules/MagnetoDynamics2D.F90
@@ -432,13 +432,13 @@ CONTAINS
 
     LOGICAL :: Cubic, HBcurve, Found, Stat
 
-    REAL(KIND=dp), POINTER :: Bval(:), Hval(:), Cval(:), &
-      CubicCoeff(:), HB(:,:)
+    REAL(KIND=dp), POINTER :: Bval(:), Hval(:), Cval(:)
+    REAL(KIND=dp), POINTER :: CubicCoeff(:) => NULL(), HB(:,:) => NULL()
     TYPE(ValueListEntry_t), POINTER :: Lst
     TYPE(ValueList_t), POINTER :: Material, BodyForce
 
     TYPE(Nodes_t), SAVE :: Nodes
-!$omp threadprivate(Nodes)
+!$omp threadprivate(Nodes, CubicCoeff, HB)
     CHARACTER(LEN=MAX_NAME_LEN) :: CoilType
     LOGICAL :: CoilBody    
     TYPE(ValueList_t), POINTER :: CompParams
@@ -447,8 +447,6 @@ CONTAINS
     REAL(KIND=dp) :: nu_tensor(2,2)
     REAL(KIND=dp) :: B_ip(2), Alocal
 !------------------------------------------------------------------------------
-    CubicCoeff => NULL()
-    HB => NULL()
     CALL GetElementNodes( Nodes,Element )
     STIFF = 0._dp
     JAC  = 0._dp