95c491076c770e80fb96f4eaea4b6bbc87644a49
juhani.kataja <juhani.kataja@csc.fi>
2018-06-05 13:04:52 +0300

    ctest: fix in ElasticBeamRestart test

--- fem/tests/ElasticBeamRestart/runtest.cmake
+++ fem/tests/ElasticBeamRestart/runtest.cmake
@@ -1,4 +1,4 @@
-INCLUDE(test_macros)
 execute_process(COMMAND ${ELMERGRID_BIN} 1 2 beam2d)
-execute_process(COMMAND ${ELMERSOLVER_BIN} ref.sif)
+# execute_process(COMMAND ${ELMERSOLVER_BIN} ref.sif)
+EXECUTE_ELMER_SOLVER(ref.sif)
 RUN_ELMER_TEST()
