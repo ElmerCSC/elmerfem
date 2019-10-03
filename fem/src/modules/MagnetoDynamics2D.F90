ElmerCSC/elmerfem

commit 5b1606dba667080786c14a25c235c65648a6e837

Fixes for the magnetization vector in 2D
- The magnetization vector in the case of axial symmetry has been corrected
- The sign of the magnetization vector is now as documented in the Elmer Models
  Manual: the RHS source is curl M (the previous implementation had -curl M)

fem/src/modules/MagnetoDynamics2D.F90
@@ -649,8 +649,11 @@ CONTAINS
         END DO
       END IF
 
-      FORCE(1:nd) = FORCE(1:nd) + IP % s(t) * DetJ * (LoadAtip * Basis(1:nd) + &
-           (M_ip(1)*dBasisdx(1:nd,2)-M_ip(2)*dBasisdx(1:nd,1)))
+      FORCE(1:nd) = FORCE(1:nd) + IP % s(t) * DetJ * (LoadAtip * Basis(1:nd) - &
+          (M_ip(1)*dBasisdx(1:nd,2)-M_ip(2)*dBasisdx(1:nd,1)))
+      IF ( CSymmetry ) THEN
+        FORCE(1:nd) = FORCE(1:nd) + IP % s(t) * DetJ * (M_ip(2)) * Basis(1:nd)/x
+      END IF
       IF(zirka) then
         FORCE(1:nd) = FORCE(1:nd) - (H_ip(1)*Bt(1:nd,1) + H_ip(2)*Bt(1:nd,2)) * IP % s(t) * detJ
       END IF
@@ -1587,8 +1590,11 @@ CONTAINS
         END DO
       END IF
 
-      FORCE(1:nd) = FORCE(1:nd) + IP % s(t) * DetJ * (LoadAtip * Basis(1:nd) + &
+      FORCE(1:nd) = FORCE(1:nd) + IP % s(t) * DetJ * (LoadAtip * Basis(1:nd) - &
            (M_ip(1)*dBasisdx(1:nd,2)-M_ip(2)*dBasisdx(1:nd,1)))
+      IF ( CSymmetry ) THEN
+        FORCE(1:nd) = FORCE(1:nd) + IP % s(t) * DetJ * (M_ip(2)) * Basis(1:nd)/x
+      END IF
     END DO
 
     IF (HBcurve.AND.NewtonRaphson) THEN

fem/tests/mgdyn2D_em/konesec.sif
@@ -147,10 +147,10 @@ Material 4
 
   $ mag = -8e5
   Magnetization 1 = Variable Coordinate
-    Real MATC "mag*mx(tx(0) tx(1))
+    Real MATC "-mag*mx(tx(0) tx(1))
 
   Magnetization 2 = Variable Coordinate
-    Real MATC "mag*mx(tx(1) tx(0))
+    Real MATC "-mag*mx(tx(1) tx(0))
 End
 
 Equation 1 :: Active Solvers(2) = 2 3

fem/tests/mgdyn2D_em_conforming/konesec.sif
@@ -152,10 +152,10 @@ Material 4
 
   $ mag = -8e5
   Magnetization 1 = Variable Coordinate
-    Real MATC "mag*mx(tx(0) tx(1))
+    Real MATC "-mag*mx(tx(0) tx(1))
 
   Magnetization 2 = Variable Coordinate
-    Real MATC "mag*mx(tx(1) tx(0))
+    Real MATC "-mag*mx(tx(1) tx(0))
 End
 
 Equation 1 :: Active Solvers(2) = 2 3

fem/tests/mgdyn2D_em_conforming/ref.sif
@@ -153,10 +153,10 @@ Material 4
 
   $ mag = -8e5
   Magnetization 1 = Variable Coordinate
-    Real MATC "mag*mx(tx(0) tx(1))
+    Real MATC "-mag*mx(tx(0) tx(1))
 
   Magnetization 2 = Variable Coordinate
-    Real MATC "mag*mx(tx(1) tx(0))
+    Real MATC "-mag*mx(tx(1) tx(0))
 End
 
 Equation 1 :: Active Solvers(2) = 2 3

fem/tests/mgdyn2D_em_harmonic/konesec.sif
@@ -149,10 +149,10 @@ Material 4
 
   $ mag = -8e5
   Magnetization 1 = Variable Coordinate
-    Real MATC "mag*mx(tx(0) tx(1))"
+    Real MATC "-mag*mx(tx(0) tx(1))"
 
   Magnetization 2 = Variable Coordinate
-    Real MATC "mag*mx(tx(1) tx(0))"
+    Real MATC "-mag*mx(tx(1) tx(0))"
 End
 
 Equation 1 :: Active Solvers(3) = 2 3 5

fem/tests/mgdyn2D_pm/ptest.sif
@@ -27,8 +27,8 @@ Material 1
 
 $ mag = 1006582
 $ ang = pi/2
-  Magnetization 1 = Real $ mag*cos(ang)
-  Magnetization 2 = Real $ mag*sin(ang)
+  Magnetization 1 = Real $ -mag*cos(ang)
+  Magnetization 2 = Real $ -mag*sin(ang)
 
   Youngs Modulus=1
   Poisson Ratio=0.35

fem/tests/mgdyn_nodalforce2d/konesec.sif
@@ -153,10 +153,10 @@ Material 4
   Relative Permittivity = 1
   $ mag = -8e5
   Magnetization 1 = Variable Coordinate
-    Real MATC "mag*mx(tx(0) tx(1))"
+    Real MATC "-mag*mx(tx(0) tx(1))"
 
   Magnetization 2 = Variable Coordinate
-    Real MATC "mag*mx(tx(1) tx(0))"
+    Real MATC "-mag*mx(tx(1) tx(0))"
 End
 
 ! nonlinear iron

commit 0d0519d10dc8a2343864366179400071ba38281a

Correct the sign of the magnetization source in Cartesian 2D coordinates
Unfortunately the commit 5b1606dba667080786c14a25c235c65648a6e837 introduced
a wrong sign for the magnetization source in the case of Cartesian 2D
coordinates. This commit doesn't change the code in the case of axial symmetry.

fem/src/modules/MagnetoDynamics2D.F90
@@ -649,10 +649,12 @@ CONTAINS
         END DO
       END IF
 
-      FORCE(1:nd) = FORCE(1:nd) + IP % s(t) * DetJ * (LoadAtip * Basis(1:nd) - &
-          (M_ip(1)*dBasisdx(1:nd,2)-M_ip(2)*dBasisdx(1:nd,1)))
       IF ( CSymmetry ) THEN
-        FORCE(1:nd) = FORCE(1:nd) + IP % s(t) * DetJ * (M_ip(2)) * Basis(1:nd)/x
+        FORCE(1:nd) = FORCE(1:nd) + IP % s(t) * DetJ * (LoadAtip * Basis(1:nd) - &
+            M_ip(1)*dBasisdx(1:nd,2)+M_ip(2)*(dBasisdx(1:nd,1) + Basis(1:nd)/x))
+      ELSE
+        FORCE(1:nd) = FORCE(1:nd) + IP % s(t) * DetJ * (LoadAtip * Basis(1:nd) + &
+            M_ip(1)*dBasisdx(1:nd,2)-M_ip(2)*dBasisdx(1:nd,1))
       END IF
       IF(zirka) then
         FORCE(1:nd) = FORCE(1:nd) - (H_ip(1)*Bt(1:nd,1) + H_ip(2)*Bt(1:nd,2)) * IP % s(t) * detJ
@@ -1590,10 +1592,12 @@ CONTAINS
         END DO
       END IF
 
-      FORCE(1:nd) = FORCE(1:nd) + IP % s(t) * DetJ * (LoadAtip * Basis(1:nd) - &
-           (M_ip(1)*dBasisdx(1:nd,2)-M_ip(2)*dBasisdx(1:nd,1)))
       IF ( CSymmetry ) THEN
-        FORCE(1:nd) = FORCE(1:nd) + IP % s(t) * DetJ * (M_ip(2)) * Basis(1:nd)/x
+        FORCE(1:nd) = FORCE(1:nd) + IP % s(t) * DetJ * (LoadAtip * Basis(1:nd) - &
+            M_ip(1)*dBasisdx(1:nd,2)+M_ip(2)*(dBasisdx(1:nd,1) + Basis(1:nd)/x))
+      ELSE
+        FORCE(1:nd) = FORCE(1:nd) + IP % s(t) * DetJ * (LoadAtip * Basis(1:nd) + &
+            M_ip(1)*dBasisdx(1:nd,2)-M_ip(2)*dBasisdx(1:nd,1))
       END IF
     END DO
 

fem/tests/mgdyn2D_em/konesec.sif
@@ -147,10 +147,10 @@ Material 4
 
   $ mag = -8e5
   Magnetization 1 = Variable Coordinate
-    Real MATC "-mag*mx(tx(0) tx(1))
+    Real MATC "mag*mx(tx(0) tx(1))
 
   Magnetization 2 = Variable Coordinate
-    Real MATC "-mag*mx(tx(1) tx(0))
+    Real MATC "mag*mx(tx(1) tx(0))
 End
 
 Equation 1 :: Active Solvers(2) = 2 3

fem/tests/mgdyn2D_em_conforming/konesec.sif
@@ -152,10 +152,10 @@ Material 4
 
   $ mag = -8e5
   Magnetization 1 = Variable Coordinate
-    Real MATC "-mag*mx(tx(0) tx(1))
+    Real MATC "mag*mx(tx(0) tx(1))
 
   Magnetization 2 = Variable Coordinate
-    Real MATC "-mag*mx(tx(1) tx(0))
+    Real MATC "mag*mx(tx(1) tx(0))
 End
 
 Equation 1 :: Active Solvers(2) = 2 3

fem/tests/mgdyn2D_em_conforming/ref.sif
@@ -153,10 +153,10 @@ Material 4
 
   $ mag = -8e5
   Magnetization 1 = Variable Coordinate
-    Real MATC "-mag*mx(tx(0) tx(1))
+    Real MATC "mag*mx(tx(0) tx(1))
 
   Magnetization 2 = Variable Coordinate
-    Real MATC "-mag*mx(tx(1) tx(0))
+    Real MATC "mag*mx(tx(1) tx(0))
 End
 
 Equation 1 :: Active Solvers(2) = 2 3

fem/tests/mgdyn2D_em_harmonic/konesec.sif
@@ -149,10 +149,10 @@ Material 4
 
   $ mag = -8e5
   Magnetization 1 = Variable Coordinate
-    Real MATC "-mag*mx(tx(0) tx(1))"
+    Real MATC "mag*mx(tx(0) tx(1))"
 
   Magnetization 2 = Variable Coordinate
-    Real MATC "-mag*mx(tx(1) tx(0))"
+    Real MATC "mag*mx(tx(1) tx(0))"
 End
 
 Equation 1 :: Active Solvers(3) = 2 3 5

fem/tests/mgdyn2D_pm/ptest.sif
@@ -27,8 +27,8 @@ Material 1
 
 $ mag = 1006582
 $ ang = pi/2
-  Magnetization 1 = Real $ -mag*cos(ang)
-  Magnetization 2 = Real $ -mag*sin(ang)
+  Magnetization 1 = Real $ mag*cos(ang)
+  Magnetization 2 = Real $ mag*sin(ang)
 
   Youngs Modulus=1
   Poisson Ratio=0.35

fem/tests/mgdyn_nodalforce2d/konesec.sif
@@ -153,10 +153,10 @@ Material 4
   Relative Permittivity = 1
   $ mag = -8e5
   Magnetization 1 = Variable Coordinate
-    Real MATC "-mag*mx(tx(0) tx(1))"
+    Real MATC "mag*mx(tx(0) tx(1))"
 
   Magnetization 2 = Variable Coordinate
-    Real MATC "-mag*mx(tx(1) tx(0))"
+    Real MATC "mag*mx(tx(1) tx(0))"
 End
 
 ! nonlinear iron