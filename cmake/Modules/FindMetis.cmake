84670b471c590ae05e6e95d52e450ff9f0482109
Foad Sojoodi Farimani <f.s.farimani@gmail.com>
2020-04-13 23:15:43 +0200

    compiling for Ubuntu

--- cmake/Modules/FindMetis.cmake
+++ cmake/Modules/FindMetis.cmake
@@ -1,66 +1,57 @@
 # CMake script for finding Metis
 
-# If libraries are already defined, do nothing 
-IF(Metis_LIBRARIES AND Metis_INCLUDE_DIR)
-  SET(Metis_FOUND TRUE)
-  RETURN()
-ENDIF()
+# If libraries are already defined, do nothing
+if(Metis_LIBRARIES AND Metis_INCLUDE_DIR)
+  set(Metis_FOUND TRUE)
+  return()
+endif()
 
-SET(Metis_FOUND FALSE)
-MESSAGE(STATUS "Finding Metis")
+set(Metis_FOUND FALSE)
+message(STATUS "Finding Metis")
 
 # Try to find Metis
-SET(METISINCLUDE 
-  "${METISROOT}/include"
-  "$ENV{METISROOT}/include"
-  "$ENV{METIS_ROOT}/include"
-  "$ENV{PARMETISROOT}/include"
-  "$ENV{PARMETIS_ROOT}/include"
-  "${CMAKE_SOURCE_DIR}/metis/include"
-  INTERNAL)
+set(METISINCLUDE
+    "${METISROOT}/include"
+    "$ENV{METISROOT}/include"
+    "$ENV{METIS_ROOT}/include"
+    "$ENV{PARMETISROOT}/include"
+    "$ENV{PARMETIS_ROOT}/include"
+    "${CMAKE_SOURCE_DIR}/metis/include"
+    INTERNAL)
 
-SET(METIS_INCLUDENAME "metis.h" "parmetis.h" INTERNAL)
-FIND_PATH(Metis_INCLUDE_DIR
-  NAMES
-  ${METIS_INCLUDENAME} 
-  HINTS 
-  ${METISINCLUDE}
-  )
+set(METIS_INCLUDENAME "metis.h" "parmetis.h" INTERNAL)
+find_path(
+  Metis_INCLUDE_DIR metis.h
+  PATHS /usr/include
+  NAMES ${METIS_INCLUDENAME}
+  HINTS ${METISINCLUDE})
 
-SET(METISLIB
-  "${METISROOT}/lib"
-  "$ENV{METISROOT}/lib"
-  "$ENV{METIS_ROOT}/lib"
-  "$ENV{PARMETISROOT}/lib"
-  "$ENV{PARMETIS_ROOT}/lib"
-  "${CMAKE_SOURCE_DIR}/metis/lib"
-  INTERNAL)
+set(METISLIB
+    "${METISROOT}/lib"
+    "$ENV{METISROOT}/lib"
+    "$ENV{METIS_ROOT}/lib"
+    "$ENV{PARMETISROOT}/lib"
+    "$ENV{PARMETIS_ROOT}/lib"
+    "${CMAKE_SOURCE_DIR}/metis/lib"
+    INTERNAL)
 
-FIND_LIBRARY(Metis_LIBRARIES 
-  metis
-  HINTS
-  ${METISLIB})
+find_library(Metis_LIBRARIES metis HINTS ${METISLIB})
 
-IF (Metis_LIBRARIES AND Metis_INCLUDE_DIR)
-  SET(Metis_FOUND TRUE)
-ENDIF()
+if(Metis_LIBRARIES AND Metis_INCLUDE_DIR)
+  set(Metis_FOUND TRUE)
+endif()
 
-IF (Metis_FOUND) 
-  IF (NOT Metis_FIND_QUIETLY)
-    MESSAGE(STATUS "A library with Metis API found.")
-    MESSAGE(STATUS "Metis include dir: ${Metis_INCLUDE_DIR}")
-    MESSAGE(STATUS "Metis libraries: ${Metis_LIBRARIES}")
-  ENDIF()
-ELSE()
-  IF (Metis_FIND_REQUIRED)
-    MESSAGE(FATAL_ERROR "Metis libraries not found.")
-  ENDIF()
-ENDIF()
+if(Metis_FOUND)
+  if(NOT Metis_FIND_QUIETLY)
+    message(STATUS "A library with Metis API found.")
+    message(STATUS "Metis include dir: ${Metis_INCLUDE_DIR}")
+    message(STATUS "Metis libraries: ${Metis_LIBRARIES}")
+  endif()
+else()
+  if(Metis_FIND_REQUIRED)
+    message(FATAL_ERROR "Metis libraries not found.")
+  endif()
+endif()
 
-MARK_AS_ADVANCED(
-  METISINCLUDE
-  METISLIB
-  METIS_INCLUDENAME
-  Metis_INCLUDE_DIR 
-  Metis_LIBRARIES 
-  )
+mark_as_advanced(METISINCLUDE METISLIB METIS_INCLUDENAME Metis_INCLUDE_DIR
+                 Metis_LIBRARIES)
