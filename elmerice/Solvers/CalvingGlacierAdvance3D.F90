b2646fc3a44c3f56d24102a722dbd9cb8124f6de
Eef van Dongen <eefvandongen@gmail.com>
2020-09-11 09:24:26 +0200

    Updating the implementation of 'rails' for glacier advance following margins. All nodes on the lateral margin and front advance now, based on a projection of the velocity onto the rails. Not checking for tangled nodes yet, so may cause convergence/ mesh quality problems in the long run.

--- elmerice/Solvers/CalvingGlacierAdvance3D.F90
+++ elmerice/Solvers/CalvingGlacierAdvance3D.F90
@@ -63,8 +63,7 @@
         FrontNodeNums(:)=>NULL(),LeftPerm(:)=>NULL(), RightPerm(:)=>NULL()
    INTEGER, ALLOCATABLE :: FrontLocalNodeNumbers(:), &
         NodeNumbers(:), TangledGroup(:), TangledPivotIdx(:), UpdatedDirection(:)
-   REAL(KIND=dp) :: FrontOrientation(3),RotationMatrix(3,3),&
-        NodeVelo(3), NodeMelt(3), NodeNormal(3), RailDir(2),&
+   REAL(KIND=dp) :: NodeVelo(3), NodeMelt(3), NodeNormal(3), RailDir(2),&
         MeltRate, Displace(3), y_coord(2), epsShift, LongRangeLimit, MaxDisplacement, &
         EpsTangle,thisEps,Shift, thisY,xx,yy,TempDist,MinDist,xt,yt,t
    REAL(KIND=dp), POINTER :: Advance(:)
@@ -118,9 +117,9 @@
       WRITE(Message,'(A,A)') 'Left Rail Number Nodes not found'
       CALL FATAL(SolverName, Message)
    END IF
-!TO DO only do these things if firsttime=true?
+   !TO DO only do these things if firsttime=true?
    OPEN(unit = io, file = TRIM(LeftRailFName), status = 'old',iostat = ok)
-print *, ok
+   print *, ok
    IF (ok /= 0) THEN
       WRITE(message,'(A,A)') 'Unable to open file ',TRIM(LeftRailFName)
       CALL FATAL(Trim(SolverName),Trim(message))
@@ -169,7 +168,6 @@ print *, ok
    END IF
    CLOSE(io)
 
-
    !Get melt rate
    MeltVarName = ListGetString(Params, "Melt Variable Name", Found)
    IF(.NOT. Found) THEN
@@ -188,18 +186,12 @@ print *, ok
    NormalVarName = ListGetString(Params, "Normal Vector Variable Name", UnfoundFatal=.TRUE.)
    NormalVar => VariableGet(Mesh % Variables, NormalVarName, .TRUE., UnfoundFatal=.TRUE.)
 
-   !Get the orientation of the calving front, compute rotation matrix
-   !TODO: generalize and link
-   FrontOrientation = GetFrontOrientation(Model)
-   RotationMatrix = ComputeRotationMatrix(FrontOrientation)
-
    MaxDisplacement = ListGetConstReal(Params, "Maximum Node Displacement", Found)
    IF(.NOT.Found) THEN
      CALL Info(SolverName, "'Maximum Node Displacement' not found, setting to 1.0E4.")
      MaxDisplacement = 1.0E4_dp
    END IF
 
- 
    !Get the front line
    FrontMaskName = "Calving Front Mask"
    TopMaskName = "Top Surface Mask"
@@ -222,8 +214,7 @@ print *, ok
    !--------------------------------------
    ! Action: Compute lagrangian displacement for all nodes
    !         This is the main function of the Solver.
-   !         Everything following this DO loop is taking care of
-   !         unprojectability/high gradient etc
+   !         TO DO: check for 'tangled' regions, see CalvingFrontAdvance3D.F90
    !--------------------------------------
     
    DO i=1,Mesh % NumberOfNodes
@@ -250,131 +241,130 @@ print *, ok
       NodeVelo(1) = VeloVar % Values(((VeloVar % Perm(i)-1)*VeloVar % DOFs) + 1)
       NodeVelo(2) = VeloVar % Values(((VeloVar % Perm(i)-1)*VeloVar % DOFs) + 2)
       NodeVelo(3) = VeloVar % Values(((VeloVar % Perm(i)-1)*VeloVar % DOFs) + 3)
-      
-      
+            
       Displace = 0.0
       IF ( FrontPerm(i) > 0 ) THEN
          Displace(1) =  NodeVelo(1) - NodeMelt(1)
          Displace(2) =  NodeVelo(2) - NodeMelt(2)
          Displace(3) =  NodeVelo(3) - NodeMelt(3)
-         IF ((LeftPerm(i)>0) .OR. (RightPerm(i)>0)) THEN   ! TO DO on left or right corner
-            ! Strategy to project displace from x to x+dx along rail direction:
-            ! - find closest rail node, then
-            ! - three cases to find which rail segment should be used:
-            ! 1) x is rail node, then just find out which segment closest to x+dx
-            ! 2) both x and x+dx are on one rail segment
-            ! 3) x is on one segment and x+dx on another
-            ! only do x,y direction; no penetration in vertical is handled by freesurf.
-            ! NOTE mass is not conserved but for small timesteps and a relatively thin lateral boundary it should be insignificant..
-            xx = Solver % Mesh % Nodes % x(i)
-            yy = Solver % Mesh % Nodes % y(i)
-            xt=xx+Displace(1)*dt
-            yt=yy+Displace(2)*dt
-            ! TO DO lot of inefficient coding here. better to assign RailX=xL if on left RailX=xR if on right and continue
-            ! Q: need to allocate/deallocate all the time? (considering case where Nl is not Nr? and set RailN each time?
-            ! IF LEFT set xrail = xL else ... etc
-            IF (LeftPerm(i)>0) THEN
-               Nrail= Nl
-               ALLOCATE(xRail(Nrail), yRail(Nrail))
-               xRail = xL
-               yRail = yL
-            ELSE
-               Nrail= Nr
-               ALLOCATE(xRail(Nrail), yRail(Nrail))
-               xRail = xR
-               yRail = yR ! TO DO use pointers instead?
+	 Displace = Displace * dt
+      END IF
+      ! NOTE: Displace overwritten on corner of left-front and right-front
+      ! melt not taken into account on those corner nodes
+      IF ((LeftPerm(i)>0) .OR. (RightPerm(i)>0)) THEN  
+         ! Strategy to project displace from x to x+v*dt along rail direction:
+         ! - find closest rail node, then find cosest rail segment.
+         ! - three cases to find which rail segment should be used:
+         ! 1) x is rail node, then just find out which segment closest to x+dt*v
+         ! 2) both x and x+dt*v are on one rail segment
+         ! 3) x is on one segment and x+dt*v on another
+         ! only do x,y direction; vertical update is handled by freesurf.
+         ! NOTE mass is not strictly conserved but for small timesteps and
+         ! a thin lateral boundary artificial mass change should be insignificant.
+         xx = Solver % Mesh % Nodes % x(i)
+         yy = Solver % Mesh % Nodes % y(i)
+         xt=xx+NodeVelo(1)*dt
+         yt=yy+NodeVelo(2)*dt
+         IF (LeftPerm(i)>0) THEN
+            Nrail= Nl
+            ALLOCATE(xRail(Nrail), yRail(Nrail))
+            xRail = xL
+            yRail = yL
+         ELSE
+            Nrail= Nr
+            ALLOCATE(xRail(Nrail), yRail(Nrail))
+            xRail = xR
+            yRail = yR ! TO DO use pointers instead?
+         END IF
+         MinDist=(xRail(1)-xRail(Nrail))**2.+(yRail(1)-yRail(Nrail))**2.
+         ! MinDist is actually maximum distance, needed for finding closest rail node
+         DO j=1,Nrail ! Find closest point on rail
+            TempDist=(xRail(j)-xx)**2.+(yRail(j)-yy)**2.
+            IF(TempDist < MinDist) THEN
+               MinDist=TempDist
+               jmin=j
             END IF
-            MinDist=(xRail(1)-xRail(Nrail))**2.+(yRail(1)-yRail(Nrail))**2.
-            DO j=1,Nrail
-               !  TO DO save jmin,k from prev time and start checking distance from there
-               ! for efficiency
-               TempDist=(xRail(j)-xx)**2.+(yRail(j)-yy)**2.
-               IF(TempDist < MinDist) THEN
-                  MinDist=TempDist
-                  jmin=j
-               END IF
-            END DO
-            IF(jmin+1>Nrail) PRINT *, 'jmin=',jmin,'ERROR: advancing beyond left rail!'
-            IF (MinDist < AEPS) THEN
-               ! case 1)  mesh node is rail point'
-               ! this likely happens at t=0.
-               MovePastRailNode=.FALSE. !if this would be true here, rail resolution is smaller than mesh resolution
-               ! we assume that's never the case. TO DO include assert  CALL Assert(1==0, SolverName, "1 does not equal zero!")
-               ! check whether x+dx*dt is closest to segment jmin+1 or jmin-1  
-               IF(PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
+         END DO
+         IF(jmin+1>Nrail) PRINT *, 'jmin=',jmin,'ERROR: advancing/retreating beyond rail!'
+         !NOTE, this assumes rails are starting at back and going towards front, should check k > 0 as well?            
+         IF (MinDist < AEPS) THEN ! min distance very close to 0
+            ! case 1)  mesh node is a rail point
+            ! this likely happens at t=0.
+            MovePastRailNode=.FALSE. !if both at a rail node and passing next rail node, rail resolution is smaller than displacement per timestep
+            ! we assume that's never the case. TO DO include assert
+            ! check whether x+v*dt is closest to segment jmin+1 or jmin-1  
+            IF(PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
+                 (/xRail(jmin+1),yRail(jmin+1)/),(/xt,yt/)) &
+                 > PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
+                 (/xRail(jmin-1),yRail(jmin-1)/),(/xt,yt/))) THEN
+               k=jmin-1 ! x+v*dt is on jmin-1 -- jmin
+            ELSE
+               k=jmin+1
+            END IF
+            PRINT *, 'NOTE! mesh node on a rail node!'
+         ELSE ! x is not a rail node, check whether x is closest to  jmin+1 or jmin-1  
+            IF(PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
+               (/xRail(jmin+1),yRail(jmin+1)/),(/xx,yy/)) &
+               > PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
+               (/xRail(jmin-1),yRail(jmin-1)/),(/xx,yy/))) THEN
+                 ! x closest to jmin-1 -- jmin segment
+                 IF (PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
+                     (/xRail(jmin+1),yRail(jmin+1)/),(/xt,yt/)) &
+                     > PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
+                     (/xRail(jmin-1),yRail(jmin-1)/),(/xt,yt/))) THEN
+                     ! x+v*dt also closest to jmin--jmin-1 
+                     MovePastRailNode=.FALSE. ! case 2) x+dt*v and x are on same segment
+                     k=jmin-1 ! x and x+v*dt are closest to jmin>jmin-1 segment
+                 ELSE
+                     ! case 3) advancing past rail segment
+                     MovePastRailNode=.TRUE. 
+                     k=jmin+1 ! x is moving from jmin-1--jmin to jmin--jmin+1
+                 END IF
+            ELSE ! x closest to jmin -- jmin+1 segment, but closer to jmin than jmin+1
+                 ! assuming not moving past jmin+1 (see above assumption on time step and rail resolution)
+                 IF (PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
                     (/xRail(jmin+1),yRail(jmin+1)/),(/xt,yt/)) &
                     > PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
                     (/xRail(jmin-1),yRail(jmin-1)/),(/xt,yt/))) THEN
-                  k=jmin-1
-                  ELSE
+                     ! Case 3) x advancing past rail segment
+                     MovePastRailNode=.TRUE.
+                     k=jmin-1 ! x is moving from jmin+1>jmin to jmin>jmin-1
+                 ELSE
+                     ! Case 2) x+v*dt and x are on same segment jmin>jmin+1     
+                     MovePastRailNode=.FALSE.
                      k=jmin+1
-                  END IF
-               ELSE ! x is not a rail node
-                  IF(PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
-                       (/xRail(jmin+1),yRail(jmin+1)/),(/xx,yy/)) &
-                       > PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
-                       (/xRail(jmin-1),yRail(jmin-1)/),(/xx,yy/))) THEN
-                     IF (PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
-                       (/xRail(jmin+1),yRail(jmin+1)/),(/xt,yt/)) &
-                       > PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
-                       (/xRail(jmin-1),yRail(jmin-1)/),(/xt,yt/))) THEN
-                        MovePastRailNode=.FALSE. ! case 2) x+dx and x are on same segment'
-                        k=jmin-1 ! x and x+dx are closest to jmin>jmin-1 segment
-                     ELSE
-                        ! case 3) advancing past rail segment
-                        MovePastRailNode=.TRUE. ! TO DO should check whether projected
-                        !displacement moves past rail node, not whether unprojected displacement does..?
-                        k=jmin+1 ! x is moving from jmin-1>jmin to jmin>jmin+1
-                     END IF
-                  ELSE
-                     IF (PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
-                       (/xRail(jmin+1),yRail(jmin+1)/),(/xt,yt/)) &
-                       > PointLineSegmDist2D((/xRail(jmin),yRail(jmin)/), &
-                       (/xRail(jmin-1),yRail(jmin-1)/),(/xt,yt/))) THEN
-                        ! Case 3) x advancing past rail segment
-                        MovePastRailNode=.TRUE.
-                        k=jmin-1 ! x is moving from jmin+1>jmin to jmin>jmin-1
-                     ELSE
-                        ! Case 2) x+dx and x are on same segment jmin>jmin+1     
-                        MovePastRailNode=.FALSE.
-                        k=jmin+1
-                     END IF
-                  END IF
-               END IF
-            IF(MovePastRailNode) THEN
-               ! We assume that the node moves from jmin>jmin-1 to jmin>jmin+1 or opposite
-               ! but we exclude moving from jmin>jmin+1 to jmin+1>jmin+2 (or with minus)
-               ! because then displacement in timestep is larger than rail resolution
-               ! TO DO check this with CALL ASSERT
-               ! we change the displacement such that the node first moves along the current rail segment
-               ! to the nearest rail node. whatever magnitude of the horizontal part of the displacement is left
-               ! will be the distance the node travels on the new segment.
-               TempDist=( (xx-xRail(jmin))**2.+(yy-yRail(jmin))**2.)**0.5 ! distance travelled to reach new rail node
-               TempDist= (Displace(1)**2.+Displace(2)**2.)**0.5 *dt - TempDist ! distance left to travel along new segment
-               t=TempDist/( (xRail(k)-xRail(jmin))**2.+(yRail(k)-yRail(jmin))**2.)**0.5 ! relative distance on segment
-               Displace(1)=t*(xRail(k)-xRail(jmin))
-               Displace(2)=t*(yRail(k)-yRail(jmin))
-               ! TO DO check if *dt should be here?! *dt is done later!!! now it's done twice!
-               ! maybe move the whole IF MovePastRailNode part down after other Displacement*dt
-               ! or put Displacement*dt up for all cases (front, and corner cases 1-3)
-               ! TO DO above procedure can be improved to first calculate norm of displacement
-               ! projected onto segm1 and then calculate how much displacement left on segm2 +
-               ! project that as well!
-            ELSE
-               RailDir=(/xRail(jmin),yRail(jmin)/)-(/xRail(k),yRail(k)/)
-               PRINT *, ' RailDir', RailDir
-            Displace(1:2) = DOT_PRODUCT(Displace(1:2),RailDir) * &
-                 RailDir / DOT_PRODUCT(RailDir,RailDir)
+                 END IF
             END IF
-            ! TO DO check whether x,y + displace*dt intersects with next line segment
-            ! if it does, find point on next line segment with distance displ*dt from current x,y and send node there
-            ! TO DO write warning if distance point to line of rail too large?
-            ! TO DO also write warning if glacier advances out of range of rails?
-            !  TO DO ASSERT that ||NewDisplace|| <= ||V||*dt    
-            DEALLOCATE(xRail,yRail)
          END IF
-         Displace = Displace * dt
-      END IF
+         IF(MovePastRailNode) THEN
+            ! k is defined such that the node should after displacement lay on jmin -- k segment
+            ! nodes first move along the current rail segment to the nearest rail node jmin.
+            ! whatever magnitude of the horizontal part of v*dt is left
+            ! will be the distance the node travels on the new segment.
+            RailDir=(/xRail(jmin),yRail(jmin)/)-(/xx,yy/) ! direction of current rail
+            Displace(1:2)=(/xRail(jmin),yRail(jmin)/)-(/xx,yy/) ! get to new rail node
+            !TempDist is total distance it would have travelled only along its original segment
+            TempDist=DOT_PRODUCT(NodeVelo(1:2),RailDir) * &
+                 (RailDir(1)**2+RailDir(2)**2)**0.5 * dt / DOT_PRODUCT(RailDir,RailDir)
+            ! t is the proportion left to travel along new segment
+            t=(TempDist - ((xx-xRail(jmin))**2.+(yy-yRail(jmin))**2.)**0.5)/TempDist
+            ! new rail direction
+            RailDir=(/xRail(jmin),yRail(jmin)/)-(/xRail(k),yRail(k)/) 
+            ! add proportion left to travel along new direction
+            Displace(1:2) = Displace(1:2)+DOT_PRODUCT(NodeVelo(1:2),RailDir) * &
+                 RailDir * dt * t / DOT_PRODUCT(RailDir,RailDir)
+         ELSE ! not moving past node, just project onto current rail
+            RailDir=(/xRail(jmin),yRail(jmin)/)-(/xRail(k),yRail(k)/)
+            Displace(1:2) = DOT_PRODUCT(NodeVelo(1:2),RailDir) * &
+             RailDir / DOT_PRODUCT(RailDir,RailDir)
+            Displace = Displace * dt
+         END IF
+         ! TO DO write warning if distance point on margin to rail segment too large?
+         ! TO DO also write warning if glacier advances out of range of rails?
+         ! TO DO ASSERT that ||NewDisplace|| <= ||V||*dt    
+         DEALLOCATE(xRail,yRail)
+     END IF
+
 
      IF(MAXVAL(Displace) > MaxDisplacement) THEN
        WRITE(Message,'(A,i0,A)') "Maximum allowable front displacement exceeded for node ",i,". Limiting..."
