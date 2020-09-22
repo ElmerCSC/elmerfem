68d1da48191ac6326952ee1140ed6873e7aad44a
Mondher CHEKKI <mondher.chekki@univ-grenoble-alpes.fr>
2020-09-22 19:37:40 +0000

    Use bash command instead of sh command

--- elmerice/Tests/MMG2D_Transient/runTest.cmake
+++ elmerice/Tests/MMG2D_Transient/runTest.cmake
@@ -2,6 +2,6 @@ INCLUDE(${TEST_SOURCE}/../test_macros.cmake)
 
 FILE(COPY ${PROJECT_SOURCE_DIR}/..//Solvers/MeshAdaptation_2D/Script_Transient.sh  DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/")
 
-EXECUTE_PROCESS(COMMAND sh ./Script_Transient.sh)
+EXECUTE_PROCESS(COMMAND bash ./Script_Transient.sh)
 
 RUN_ELMERICE_TEST()
