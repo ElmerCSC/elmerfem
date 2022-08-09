4ad2a07be7b1a0ee577eedabaaa74c1637a796d5
t7saeki <53612710+t7saeki@users.noreply.github.com>
2022-08-09 18:40:41 +0900

    Some modification in accordance with Oxygen icons licensing requirement
    - All the Oxygen icons are to be installed. This is to avoid that a subset of icons is treated as "modified version" of Oxygen icons though I'm not sure that is true.
    -  Added GPLv3 text to license_texts/Oxygen-cons.txt. Note that this does not mean to distribute ElmerGUI under GPLv3.

---
+++ ElmerGUI/Application/icons/oxygen/22x22/actions/copy_icons.bat
@@ -0,0 +1,277 @@
+copy system-reboot.png ..
+copy application-exit.png ..
+copy archive-extract.png ..
+copy archive-insert-directory.png ..
+copy archive-insert.png ..
+copy archive-remove.png ..
+copy arrow-down-double.png ..
+copy arrow-down.png ..
+copy arrow-left-double.png ..
+copy arrow-left.png ..
+copy arrow-right-double.png ..
+copy arrow-right.png ..
+copy arrow-up-double.png ..
+copy arrow-up.png ..
+copy bookmark-new-list.png ..
+copy bookmark-new.png ..
+copy bookmark-toolbar.png ..
+copy character-set.png ..
+copy chronometer.png ..
+
+copy configure-shortcuts.png ..
+copy configure-toolbars.png ..
+copy configure.png ..
+copy contact-new.png ..
+copy dialog-cancel.png ..
+copy dialog-close.png ..
+copy dialog-ok-apply.png ..
+copy dialog-ok.png ..
+
+
+copy document-decrypt.png ..
+copy document-encrypt.png ..
+copy document-export.png ..
+copy document-import.png ..
+copy document-new.png ..
+copy document-open-folder.png ..
+copy document-open-recent.png ..
+copy document-open-remote.png ..
+copy document-open.png ..
+copy document-preview-archive.png ..
+copy document-preview.png ..
+copy document-print-frame.png ..
+copy document-print-preview.png ..
+copy document-print.png ..
+copy document-properties.png ..
+copy document-revert.png ..
+copy document-save-all.png ..
+copy document-save-as.png ..
+copy document-save.png ..
+copy document-sign.png ..
+copy draw-brush.png ..
+copy draw-eraser.png ..
+copy draw-freehand.png ..
+copy draw-text.png ..
+copy edit-clear-history.png ..
+copy edit-clear-list.png ..
+copy edit-clear-locationbar-ltr.png ..
+copy edit-clear-locationbar-rtl.png ..
+copy edit-clear.png ..
+copy edit-copy.png ..
+copy edit-cut.png ..
+
+copy edit-delete-shred.png ..
+copy edit-delete.png ..
+
+
+copy edit-find.png ..
+copy edit-paste.png ..
+copy edit-redo.png ..
+copy edit-rename.png ..
+copy edit-select-all.png ..
+copy edit-undo.png ..
+copy feed-subscribe.png ..
+
+
+
+
+
+
+
+copy folder-new.png ..
+copy fork.png ..
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+copy get-hot-new-stuff.png ..
+copy go-bottom.png ..
+copy go-down-search.png ..
+copy go-down.png ..
+copy go-first.png ..
+copy go-home.png ..
+copy go-jump-locationbar.png ..
+copy go-jump-today.png ..
+copy go-jump.png ..
+copy go-last.png ..
+copy go-next.png ..
+copy go-previous.png ..
+copy go-top.png ..
+copy go-up-search.png ..
+copy go-up.png ..
+copy help-about.png ..
+copy help-contents.png ..
+copy help-contextual.png ..
+copy help-hint.png ..
+copy insert-image.png ..
+copy insert-table.png ..
+copy insert-text.png ..
+copy list-add-font.png ..
+
+copy list-add.png ..
+
+copy list-remove.png ..
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+copy network-connect.png ..
+copy network-disconnect.png ..
+copy news-subscribe.png ..
+copy news-unsubscribe.png ..
+
+
+
+
+copy player-time.png ..
+copy player-volume.png ..
+
+copy project-open.png ..
+copy rating.png ..
+copy roll.png ..
+copy run-build-file.png ..
+copy run-build.png ..
+copy select-rectangular.png ..
+copy services.png ..
+
+
+copy show-menu.png ..
+copy speaker.png ..
+copy system-lock-screen.png ..
+copy system-log-out.png ..
+
+copy system-run.png ..
+copy system-search.png ..
+copy system-shutdown.png ..
+copy system-suspend-hibernate.png ..
+copy system-suspend.png ..
+
+copy tab-close.png ..
+copy tab-detach.png ..
+copy tab-duplicate.png ..
+copy tab-new-background.png ..
+copy tab-new.png ..
+copy text-speak.png ..
+copy tools-check-spelling.png ..
+
+
+copy tools-report-bug.png ..
+copy tools-wizard.png ..
+copy transform-move.png ..
+copy transform-rotate.png ..
+copy trash-empty.png ..
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+copy view-choose.png ..
+copy view-close.png ..
+copy view-filter.png ..
+copy view-fullscreen.png ..
+copy view-history.png ..
+copy view-left-close.png ..
+copy view-list-details.png ..
+
+copy view-list-text.png ..
+copy view-list-tree.png ..
+
+
+
+
+
+
+
+
+
+
+
+
+
+copy view-presentation.png ..
+copy view-preview.png ..
+
+
+
+
+
+copy view-refresh.png ..
+copy view-restore.png ..
+copy view-right-close.png ..
+copy view-right-new.png ..
+copy view-sidetree.png ..
+copy view-sort-ascending.png ..
+copy view-sort-descending.png ..
+copy view-split-left-right.png ..
+copy view-split-top-bottom.png ..
+copy view-statistics.png ..
+copy view-web-browser-dom-tree.png ..
+copy window-close.png ..
+copy window-duplicate.png ..
+copy window-new.png ..
+copy window-suppressed.png ..
+copy zoom-fit-best.png ..
+copy zoom-in.png ..
+copy zoom-original.png ..
+copy zoom-out.png ..
+
+
