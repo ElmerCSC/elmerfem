90e0e1800a34302946030d96bae8cc5a6aba99cd
Juha Ruokolainen <jpr@ruokolainen-linux.x.csc.fi>
2022-09-29 11:32:39 +0300

    ...

--- fem/src/modules/CahnHillard.F90
+++ fem/src/modules/CahnHillard.F90
@@ -143,7 +143,7 @@ CONTAINS
 
     Velo = 0._dp
     DO i=1,dim
-      Velo_n(i,:) = GetReal( Material, 'Convection Velocity', Found )
+      Velo_n(i,:) = GetReal( Material, 'Convection Velocity '//TRIM(i2s(i)), Found )
     END DO
 
     h_n = GetReal( Material, 'Interface Thickness',Found)
