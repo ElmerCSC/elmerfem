eedb4e75f821e5144229dc25d336164761d7c524
Mika Malinen <mika.malinen@csc.fi>
2022-11-07 13:13:46 +0200

    Add missing information about the license

--- fem/src/InterpolateMeshToMesh.F90
+++ fem/src/InterpolateMeshToMesh.F90
@@ -1,6 +1,41 @@
+!/*****************************************************************************/
+! *
+! *  Elmer, A Finite Element Software for Multiphysical Problems
+! *
+! *  Copyright 1st April 1995 - , CSC - IT Center for Science Ltd., Finland
+! * 
+! *  This library is free software; you can redistribute it and/or
+! *  modify it under the terms of the GNU Lesser General Public
+! *  License as published by the Free Software Foundation; either
+! *  version 2.1 of the License, or (at your option) any later version.
+! *
+! *  This library is distributed in the hope that it will be useful,
+! *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+! *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+! *  Lesser General Public License for more details.
+! * 
+! *  You should have received a copy of the GNU Lesser General Public
+! *  License along with this library (in file ../LGPL-2.1); if not, write 
+! *  to the Free Software Foundation, Inc., 51 Franklin Street, 
+! *  Fifth Floor, Boston, MA  02110-1301  USA
+! *
+! *****************************************************************************/
+!
+!/******************************************************************************
+! *
+! *  Authors: Juha Ruokolainen, Peter Råback, Joe Todd, Mika Malinen
+! *  Email:   Juha.Ruokolainen@csc.fi
+! *  Web:     http://www.csc.fi/elmer
+! *  Address: CSC - IT Center for Science Ltd.
+! *           Keilaranta 14
+! *           02101 Espoo, Finland 
+! *
+! *
+! ******************************************************************************/
+
 
 !------------------------------------------------------------------------------
-!> Map results from mesh to mesh. The from-Mesh is stored in an octree from 
+!> Map results from mesh to mesh. The from-mesh is stored in an octree from 
 !> which it is relatively fast to find the to-nodes. When the node is found
 !> interpolation is performed. Optionally there may be an existing projector
 !> that speeds up the interpolation.
