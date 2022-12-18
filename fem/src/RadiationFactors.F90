4435a39af1ece0169b7cdfe4402027a24103122a
Juha Ruokolainen <jpr@ruokolainen-linux.x.csc.fi>
2022-12-18 15:57:59 +0200

    ...

--- fem/src/RadiationFactors.F90
+++ fem/src/RadiationFactors.F90
@@ -1754,7 +1754,7 @@
              END IF
 
              IF (AccurateNewton ) THEN
-               RHS_d(i) = 4*RHS(i)/Temp - q*e_d * black**4 ! + the nonlinear term -A'*J
+               RHS_d(i) = 4*RHS(i)/Temp - q*RelAreas(i)*e_d*Black**4 ! + the nonlinear term -A'*J
              END IF
            END IF
 
