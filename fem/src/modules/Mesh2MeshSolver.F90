3e7e898412529abfcff86aac73ced3fa22fc8c30
Peter Raback <peter.raback@csc.fi>
2023-01-10 13:20:52 +0200

    Tentative boundary smoothing strategy for crappy meshes.

--- fem/src/modules/Mesh2MeshSolver.F90
+++ fem/src/modules/Mesh2MeshSolver.F90
@@ -39,7 +39,7 @@
 !> \ingroup Solvers
 !> \{
 !------------------------------------------------------------------------------
-SUBROUTINE Mesh2MeshSolver_init0( Model,Solver,dt,TransientSimulation )
+SUBROUTINE Mesh2MeshSolver_init0( Model,Solver,dt,Transient )
 !------------------------------------------------------------------------------
   USE DefUtils
 
@@ -48,7 +48,7 @@ SUBROUTINE Mesh2MeshSolver_init0( Model,Solver,dt,TransientSimulation )
   TYPE(Solver_t) :: Solver
   TYPE(Model_t) :: Model
   REAL(KIND=dp) :: dt
-  LOGICAL :: TransientSimulation
+  LOGICAL :: Transient
 !------------------------------------------------------------------------------
 ! Local variables
 !------------------------------------------------------------------------------
@@ -66,7 +66,7 @@ END SUBROUTINE Mesh2MeshSolver_Init0
 !------------------------------------------------------------------------------
 !> Interpolates fields from one mesh toanother. 
 !------------------------------------------------------------------------------
-SUBROUTINE Mesh2MeshSolver( Model,Solver,dt,TransientSimulation )
+SUBROUTINE Mesh2MeshSolver( Model,Solver,dt,Transient )
 !------------------------------------------------------------------------------
   USE DefUtils
   
@@ -80,7 +80,7 @@ SUBROUTINE Mesh2MeshSolver( Model,Solver,dt,TransientSimulation )
   TYPE(Solver_t) :: Solver
   TYPE(Model_t) :: Model
   REAL(KIND=dp) :: dt
-  LOGICAL :: TransientSimulation
+  LOGICAL :: Transient
 !------------------------------------------------------------------------------
 ! Local variables
 !------------------------------------------------------------------------------
@@ -144,6 +144,12 @@ SUBROUTINE Mesh2MeshSolver( Model,Solver,dt,TransientSimulation )
     END IF
   END IF
 
+
+  IF( ListGetLogical( Params,'Boundary Smoother', Found ) ) THEN
+    CALL BoundarySmoother( Model,Solver,dt,Transient)
+  END IF
+
+  
   CALL Info('Mesh2MeshSolver','Target mesh name is: '//TRIM(TargetMesh % Name),Level=7)   
 
   AdditiveMode = .FALSE.
@@ -294,6 +300,252 @@ SUBROUTINE Mesh2MeshSolver( Model,Solver,dt,TransientSimulation )
   END IF
     
   CALL Info('Mesh2MeshSolver','Interpolated variables between meshes',Level=7)
+
+CONTAINS
+
+
+  !------------------------------------------------------------------------------
+  !> A smoother that only uses topologilcal information of the mesh to detect the
+  !> nodes that are not well joined.
+  !------------------------------------------------------------------------------
+  SUBROUTINE BoundarySmoother( Model,Solver,dt,Transient)
+    !------------------------------------------------------------------------------
+    USE DefUtils
+    IMPLICIT NONE
+    !------------------------------------------------------------------------------
+    TYPE(Solver_t) :: Solver
+    TYPE(Model_t) :: Model
+    REAL(KIND=dp) :: dt
+    LOGICAL :: Transient
+    !------------------------------------------------------------------------------
+    ! Local variables
+    !------------------------------------------------------------------------------
+    TYPE(Element_t),POINTER :: Element
+    TYPE(ValueList_t), POINTER :: Params
+    TYPE(Mesh_t), POINTER :: Mesh
+    TYPE(Variable_t), POINTER :: Var
+    INTEGER :: n,m,i,j,k,ii,jj,t,NoVar,BoundaryNodes,MaxDist,Loop,OperInd
+    LOGICAL :: Visited=.FALSE., Found, Ready, Parallel
+    INTEGER, POINTER :: BoundaryPerm(:), TopoDist(:), MinFriend(:), MaxFriend(:)
+    INTEGER, POINTER :: TopoA(:), TopoB(:) 
+    INTEGER, POINTER :: Indexes(:)
+    REAL(KIND=dp) :: dx
+    CHARACTER(LEN=MAX_NAME_LEN) :: VarName, OperName, Name
+    TYPE (ParallelInfo_t), POINTER :: ParallelInfo
+    CHARACTER(*), PARAMETER :: Caller = 'BoundarySmoother'
+    !------------------------------------------------------------------------------
+
+    CALL Info(Caller,'-------------------------------------------',Level=5)
+    CALL Info(Caller,'Smoothing boundary',Level=5)
+    CALL Info(Caller,'-------------------------------------------',Level=5)
+
+    Mesh => GetMesh()
+    Params => GetSolverParams()
+
+    !For the moment the parallel communication is not activated
+#define PARCOMP 0
+
+    ParallelInfo => Mesh % ParallelInfo
+    Parallel = ( ParEnv % PEs > 1) .AND. (.NOT. Mesh % SingleMesh)
+
+    Visited = .TRUE.
+
+    m = Solver % Mesh % NumberOfNodes
+    ALLOCATE( BoundaryPerm(m) )
+    BoundaryPerm = 0
+    BoundaryNodes = 0
+    CALL MakePermUsingMask( Model,Solver,Mesh,'smooth boundary', &
+        .FALSE., BoundaryPerm, BoundaryNodes, ParallelComm = Parallel ) 
+
+    ALLOCATE( TopoDist(m), MaxFriend(m), TopoA(m), TopoB(m) )
+
+    MaxFriend = 0
+
+    TopoDist = m
+    WHERE(BoundaryPerm > 0)
+      TopoDist = 0
+    END WHERE
+
+    j = ( COUNT(TopoDist == 0) )
+
+    Ready = .FALSE.
+    Loop = 0
+    DO Loop = 1, m
+      Ready = .TRUE.
+
+      DO t = 1, Mesh % NumberOfBulkElements
+        Element => Mesh % Elements(t)
+        n = Element % Type % NumberOfNodes
+        Indexes => Element % NodeIndexes
+        DO i=1,n
+          k = Indexes(i) 
+          DO j=1,n
+            IF(i==j) CYCLE
+            IF(TopoDist(k) > TopoDist(Indexes(j))+1) THEN
+              TopoDist(k) = TopoDist(Indexes(j))+1
+              Ready = .FALSE.
+            END IF
+          END DO
+        END DO
+      END DO
+
+#if PARCOMP
+      IF( Parallel ) THEN
+        CALL CommunicateParallelSystemTag(ParallelInfo,Itag = TopoDist,ParOper=1)
+        i = 0
+        IF( Ready ) i = 1
+        i = ParallelReduction(i,1)
+        IF(i==0) Ready = .FALSE.
+      END IF
+#endif
+      
+      IF( Ready ) EXIT
+    END DO
+    CALL Info(Caller,'Mesh coloring loops: '//TRIM(I2S(Loop)),Level=6)
+
+
+    MaxDist = MAXVAL( TopoDist ) 
+    CALL Info(Caller,'Maximum topology distance: '//TRIM(I2S(MaxDist)))
+
+    DO i=0,MIN(10,MaxDist)
+      j = COUNT(TopoDist==i)
+      IF(j==0) EXIT
+      CALL Info(Caller,'Node count with topology distance '//TRIM(I2S(i))//': '//TRIM(I2S(j)))
+    END DO
+
+    MaxFriend = 0
+    DO t = 1, Mesh % NumberOfBulkElements
+      Element => Mesh % Elements(t)        
+      Indexes => Element % NodeIndexes
+      n = Element % TYPE % NumberOfNodes
+      DO i=1,n
+        ii = Indexes(i)
+        DO j=1,n
+          IF(i==j) CYCLE
+          jj = Indexes(j)
+          MaxFriend(ii) = MAX(MaxFriend(ii),TopoDist(jj))
+        END DO
+      END DO
+    END DO
+
+#if PARCOMP
+    IF( Parallel ) THEN
+      CALL CommunicateParallelSystemTag(ParallelInfo,Itag = MaxFriend,ParOper=2)
+    END IF
+#endif
+    
+    DO i=0,5
+      j = COUNT(MaxFriend==i)
+      CALL Info(Caller,'Node count with max friend '//TRIM(I2S(i))//': '//TRIM(I2S(j)))
+    END DO
+
+    DO i=0,4
+      DO j=0,4
+        k = COUNT(TopoDist==i .AND. MaxFriend == j )
+        IF(k>0) CALL Info(Caller,'Topo dist '//TRIM(I2S(i))//' with max friend '//TRIM(I2S(j))//': '//TRIM(I2S(k)))
+      END DO
+    END DO
+
+    TopoA = 0
+    TopoB = 0
+    DO t = 1, Mesh % NumberOfBulkElements
+      Element => Mesh % Elements(t)        
+      Indexes => Element % NodeIndexes
+      n = Element % TYPE % NumberOfNodes
+      DO i=1,n
+        ii = Indexes(i)
+        DO j=1,n
+          IF(i==j) CYCLE
+          jj = Indexes(j)
+          TopoA(ii) = TopoA(ii) + TopoDist(jj)
+          TopoB(ii) = TopoB(ii) + TopoDist(jj)*MaxFriend(jj)**3
+        END DO
+      END DO
+    END DO
+
+    DO i=0,10
+      j = COUNT(TopoA==i)
+      IF(j==0) CYCLE
+      CALL Info(Caller,'Topo measure A '//TRIM(I2S(i))//': '//TRIM(I2S(j)))
+    END DO
+
+    DO i=0,10
+      j = COUNT(TopoB==i)
+      IF(j==0) CYCLE
+      CALL Info(Caller,'Topo measure B '//TRIM(I2S(i))//': '//TRIM(I2S(j)))
+    END DO
+
+    ! If we have "exported variables" for the fields then we can visualize them.
+    Var => VariableGet( Mesh % Variables,'Topo Dist')
+    IF( ASSOCIATED(Var) ) Var % Values = 1.0_dp * TopoDist
+
+    Var => VariableGet( Mesh % Variables,'Max Topo Dist')
+    IF( ASSOCIATED(Var) ) Var % Values = 1.0_dp * MaxFriend 
+
+    Var => VariableGet( Mesh % Variables,'Topo A')
+    IF( ASSOCIATED(Var) ) Var % Values = 1.0_dp * TopoA
+
+    j = MINVAL(TopoB)
+    Var => VariableGet( Mesh % Variables,'Topo B')
+    IF( ASSOCIATED(Var) ) Var % Values = 1.0_dp * TopoB 
+
+    NoVar = 0
+    DO WHILE(.TRUE.)    
+      NoVar = NoVar + 1
+
+      WRITE (Name,'(A,I0)') 'Smooth Variable ',NoVar  
+      VarName = ListGetString( Params, TRIM(Name), Found )
+      IF(.NOT. Found ) EXIT
+
+      CALL Info(Caller,'Smoothing variable '//TRIM(VarName))
+
+      Var => VariableGet( Mesh % Variables, VarName )
+      IF(.NOT. ASSOCIATED(Var) ) THEN
+        CALL Fatal(Caller,'Could not access variable!')
+      END IF
+      
+      ! Currently only one simple smoother     
+      !WRITE (Name,'(A,I0)') 'Operator ',NoVar
+      !OperName = ListGetString(Params,TRIM(Name),UnfoundFatal=.TRUE.)
+      !CALL Info(Caller,'Smoothing variable "'//TRIM(VarName)//'" with operator "'//TRIM(OperName)//'"',Level=5)
+      !OperInd = ListGetInteger(Params,TRIM(Name),UnfoundFatal=.TRUE.)
+      !CALL Info(Caller,'Smoothing variable "'//TRIM(VarName)//'" with operator: '//I2S(OperInd),Level=5)
+
+      DO t=1,Mesh % NumberOfBulkElements
+        Element => Mesh % Elements(t)
+        Indexes => Element % NodeIndexes
+        n = Element % TYPE % NumberOfNodes
+        DO i = 1,n        
+          ii = Indexes(i)
+          ! If TopoB==2 then we have a node that is only attached to one inland node that
+          ! has no neighbours deeper inland. 
+          IF(TopoB(ii) > 2) CYCLE 
+
+          DO j=1,n
+            jj = Indexes(j)
+            IF(TopoDist(jj) == 0) CYCLE
+            ! We are only taking the value, so no need to average or book keep etc. 
+            IF( ASSOCIATED( Var % Perm ) ) THEN
+              dx = Var % Values(Var % Perm(jj)) - Var % Values(Var % Perm(ii))
+              IF( ABS(dx) > EPSILON(dx) ) THEN
+                !PRINT *,'copy:',ii,jj,Var % Values(Var % Perm(ii)), dx
+                Var % Values(Var % Perm(ii)) = Var % Values(Var % Perm(jj))
+              END IF
+            ELSE
+              Var % Values(ii) = Var % Values(jj)
+            END IF
+          END DO
+        END DO
+      END DO
+    END DO
+
+    CALL Info(Caller,'Smoothing done!')
+    
+    DEALLOCATE(BoundaryPerm, TopoDist, MaxFriend, TopoA, TopoB ) 
+    
+    !------------------------------------------------------------------------------
+  END SUBROUTINE BoundarySmoother
+  !------------------------------------------------------------------------------
   
 !------------------------------------------------------------------------------
 END SUBROUTINE Mesh2MeshSolver
