9dff316170c207afaf0a43484839c85271326a32
tzwinger <zwingerthomas@gmail.com>
2023-11-22 16:48:19 +0200

    Added possibility to limit the effective pressure in GlaDS channel evolution to positive values

--- elmerice/Solvers/GlaDSCoupledSolver.F90
+++ elmerice/Solvers/GlaDSCoupledSolver.F90
@@ -102,7 +102,7 @@
      LOGICAL :: Found, FluxBC, Channels, Storage, FirstTime = .TRUE., &
           AllocationsDone = .FALSE.,  SubroutineVisited = .FALSE., &
           meltChannels = .TRUE., NeglectH = .TRUE., Calving = .FALSE., &
-          CycleElement=.FALSE., MABool = .FALSE., MHBool = .FALSE. 
+          CycleElement=.FALSE., MABool = .FALSE., MHBool = .FALSE., LimitEffPres=.FALSE.
      LOGICAL, ALLOCATABLE ::  IsGhostNode(:), NoChannel(:), NodalNoChannel(:)
 
      REAL(KIND=dp) :: NonlinearTol, dt, CumulativeTime, RelativeChange, &
@@ -151,7 +151,7 @@
           CCw, lc, Lw, NoChannel, NodalNoChannel, &
           Channels, meltChannels, NeglectH, BDForder, &
           Vvar, ublr, hr2, Refq, Nel,&
-          Calving, Load_h
+          Calving, Load_h, LimitEffPres
 
       
      totst = 0.0_dp
@@ -425,6 +425,10 @@
      IF ((.NOT. MABool)) CALL WARN(SolverName,'No max channel area specified. &
           Channels may grow very large')
 
+     LimitEffPres = GetLogical( SolverParams, &
+          'Limit Negative Effective Pressure', Found)
+     IF (.NOT.Found) LimitEffPres= .FALSE.
+     
      MaxH  = GetConstReal( SolverParams, &
           'Max Sheet Thickness',    MHBool )
      IF ((.NOT. MHBool)) CALL WARN(SolverName,'No max sheet thickness specified.&
@@ -1341,7 +1345,7 @@
                          ThickSolution(ThickPerm(Edge % NodeIndexes(1:n))), &
                          alphac, betac, ChannelConductivity, Phi0, Phim, Ac, lc, ng, &
                          SheetConductivity, alphas, betas, Afactor, Bfactor, &
-                         EdgeTangent, Edge, n, EdgeNodes )
+                         EdgeTangent, Edge, n, EdgeNodes, LimitEffPres)
                  ELSE
                     WRITE(Message,'(A)')' Work only for cartesian coordinate'
                     CALL FATAL( SolverName, Message)
@@ -2007,7 +2011,7 @@ END SUBROUTINE ChannelCompose
 SUBROUTINE GetEvolveChannel(ALPHA, BETA, Qcc, CArea, NodalHydPot, NodalH, &
       NodalAlphac, NodalBetac, NodalKc, NodalPhi0, NodalPhim, NodalAc, Nodallc, Nodalng, &
       NodalKs, NodalAlphas, NodalBetas, NodalAfactor, NodalBfactor, &
-      Tangent, Element, n, Nodes )
+      Tangent, Element, n, Nodes, LimitEffPres)
 !------------------------------------------------------------------------------
   USE MaterialModels
   USE Integration
@@ -2028,6 +2032,8 @@ SUBROUTINE GetEvolveChannel(ALPHA, BETA, Qcc, CArea, NodalHydPot, NodalH, &
      TYPE(Nodes_t) :: Nodes
      TYPE(Element_t), POINTER :: Element
 
+     LOGICAL :: LimitEffPres
+
 !------------------------------------------------------------------------------
 !    Local variables
 !------------------------------------------------------------------------------
@@ -2046,7 +2052,7 @@ SUBROUTINE GetEvolveChannel(ALPHA, BETA, Qcc, CArea, NodalHydPot, NodalH, &
      TYPE(GaussIntegrationPoints_t), TARGET :: IntegStuff
      REAL(KIND=dp) :: s, u, v, w
 
-     REAL(KIND=dp) :: Phi0, PhiG, Afactor, Bfactor, GradPhim, dPw, Ffactor
+     REAL(KIND=dp) :: Phi0, PhiG, EffPressatIP, Afactor, Bfactor, GradPhim, dPw, Ffactor
 
      REAL(KIND=dp) :: GradPhi, Ngrad, nbc, hsheet, nas, nbs, nac, ng, qc, Kc, Ks, lc
      
@@ -2118,8 +2124,15 @@ SUBROUTINE GetEvolveChannel(ALPHA, BETA, Qcc, CArea, NodalHydPot, NodalH, &
 
        ng = SUM(NodalNg(1:n)*Basis(1:n))
        Vc = SUM(NodalAc(1:n)*Basis(1:n))
-       Vc = Vc*ABS(Phi0-PhiG)**(ng-1.0_dp)
-       Vc = Vc*(Phi0-PhiG)
+
+       IF (LimitEffPres) THEN
+         EffPressatIP = MAX(Phi0-PhiG, 0.0_dp)
+       ELSE
+         EffPressatIP = Phi0-PhiG
+       END IF
+       
+       Vc = Vc*ABS(EffPressatIP)**(ng-1.0_dp)
+       Vc = Vc*(EffPressatIP)
 
        Afactor = SUM(NodalAfactor(1:n)*Basis(1:n))
        Bfactor = SUM(NodalBfactor(1:n)*Basis(1:n))
