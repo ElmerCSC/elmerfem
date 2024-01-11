45f45759b555c01d372b487d1972121fdb0f7696
Juha Ruokolainen <jpr@ninja-login.lnx.csc.fi>
2024-01-11 14:29:28 +0200

    Change to 64-bit int CHOLMOD interface to enable the use of GPU's.

--- fem/src/cholmod.c
+++ fem/src/cholmod.c
@@ -18,17 +18,31 @@ cholmod STDCALLBULL *FC_FUNC_(cholmod_ffactorize,CHOLMOD_FFACTORIZE)(int *n,int 
 {
   static double *y;
   int i,j,nz,ok,*p,*q;
+  int64_t *rr, *ri;
   double *xx,*bx;
 
   cholmod *handle;
 
   handle = (cholmod *)calloc(sizeof(cholmod),1);
-  cholmod_start(&handle->c);
 
+  cholmod_l_start(&handle->c);
   handle->a.nrow=*n;
   handle->a.ncol=*n;
+
+#if 0
   handle->a.p=rows;
   handle->a.i=cols;
+#else
+  rr = (int64_t *)malloc(sizeof(int64_t)*(*n+1));
+  ri = (int64_t *)malloc(sizeof(int64_t)*rows[*n]);
+
+  for(i=0; i<*n+1; i++ ) rr[i]=rows[i];
+  for(i=0; i<rows[*n]; i++ ) ri[i]=cols[i];
+
+  handle->a.p=rr;
+  handle->a.i=ri;
+#endif
+
   handle->a.x=vals;
   handle->a.packed=1;
   handle->a.sorted=1;
@@ -36,8 +50,8 @@ cholmod STDCALLBULL *FC_FUNC_(cholmod_ffactorize,CHOLMOD_FFACTORIZE)(int *n,int 
   handle->a.nzmax=rows[*n];
   handle->a.xtype=CHOLMOD_REAL;
 
-  handle->l=cholmod_analyze(&handle->a,&handle->c);
-  cholmod_factorize(&handle->a,handle->l,&handle->c);
+  handle->l=cholmod_l_analyze(&handle->a,&handle->c);
+  cholmod_l_factorize(&handle->a,handle->l,&handle->c);
 
   return handle;
 }
@@ -48,23 +62,25 @@ void STDCALLBULL FC_FUNC_(cholmod_fsolve,CHOLMOD_FSOLVE)(cholmod **handle,int *n
   int i;
   cholmod_dense *dx, *db;
 
-  db = cholmod_zeros((*handle)->a.nrow, 1, (*handle)->a.xtype, &(*handle)->c);
+  db = cholmod_l_zeros((*handle)->a.nrow, 1, (*handle)->a.xtype, &(*handle)->c);
   bb=db->x;
   for(i=0;i<*n;i++) bb[i]=b[i];
 
-  dx=cholmod_solve(CHOLMOD_A,(*handle)->l,db,&(*handle)->c);
+  dx=cholmod_l_solve(CHOLMOD_A,(*handle)->l,db,&(*handle)->c);
   xx=dx->x;
   for(i=0;i<*n;i++) x[i]=xx[i];
 
-  cholmod_free_dense(&dx, &(*handle)->c);
-  cholmod_free_dense(&db, &(*handle)->c);
+  cholmod_l_free_dense(&dx, &(*handle)->c);
+  cholmod_l_free_dense(&db, &(*handle)->c);
 }
 
 
 void STDCALLBULL FC_FUNC_(cholmod_ffree,CHOLMOD_FFREE)(cholmod **handle)
 {
-  cholmod_free_factor(&(*handle)->l,&(*handle)->c);
-  cholmod_finish(&(*handle)->c);
+  cholmod_l_free_factor(&(*handle)->l,&(*handle)->c);
+  if ((*handle)->a.p) free((*handle)->a.p);
+  if ((*handle)->a.i) free((*handle)->a.i);
+  cholmod_l_finish(&(*handle)->c);
   free(*handle);
 }
 
