f9d7e21d53c31b8282f1f1930e56bd2eedc400df
Markus Mützel <markus.muetzel@gmx.de>
2024-07-18 16:49:41 +0200

    Fix compatibility with newer versions of UMFPACK.
    In old versions of UMFPACK (like the one that is bundled with ElmerFEM),
    the `*l_*` versions of its functions used `long` for indices in their
    interface.
    
    That was later changed to use `SuiteSparse_long`. `SuiteSparse_long` was
    `long long` on Windows and `long` on any other platform.
    
    For the latest versions of UMFPACK, `SuiteSparse_long` is `int64_t` (on
    all platforms).
    
    This change fixes compilation errors on Windows (where `long` is 32-bit
    wide also on 64-bit, LLP64) and macOS (where `int64_t` is `long long`
    which has the same size as `long` but is still technically a different
    type than `long`).
    The change still maintains compatibility with old versions of UMFPACK
    (like the one that is bundled with ElmerFEM).

--- fem/config.h.cmake
+++ fem/config.h.cmake
@@ -141,6 +141,9 @@
 /* Define if you have a UMFPACK library. */
 #define HAVE_UMFPACK
 
+/* Define to the Fortran type that matches SuiteSparse_long. */
+#cmakedefine UMFPACK_LONG_FORTRAN_TYPE @UMFPACK_LONG_FORTRAN_TYPE@
+
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H
 
