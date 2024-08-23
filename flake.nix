9dd47f3095a53f3e2cb08204fc0fdbe21ae0b7ba
Matias Zwinger <matias.zwinger@csc.fi>
2024-08-23 12:56:25 +0300

    Switch to a stable nixpkgs branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Elmer FEM";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     flake-utils.url = "github:numtide/flake-utils";
 
