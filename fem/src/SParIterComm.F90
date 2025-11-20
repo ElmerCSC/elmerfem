f97269433c7fb78acddbf1da4bb2e08e8c1f0612
Benjamin Rodenberg <rodenberg@dkrz.de>
2025-11-20 11:35:33 +0100

    Add safeguard.

--- fem/src/SParIterComm.F90
+++ fem/src/SParIterComm.F90
@@ -89,6 +89,7 @@ MODULE SParIterComm
   ! prefer mpi_handshake from YAC if HAVE_YAC
   USE elmer_coupling, ONLY: mpi_handshake, MAX_GROUPNAME_LEN
 # elif defined(HAVE_XIOS)
+#   error "The mpi_handshake implementation in XIOS currently has a bug."
   ! use mpi_handshake from XIOS if only HAVE_XIOS used without HAVE_YAC
   USE XIOS, ONLY: mpi_handshake => xios_mpi_handshake, &
               MAX_GROUPNAME_LEN => xios_MAX_GROUPNAME_LEN
