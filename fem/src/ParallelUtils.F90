4f384612f2cb261677bcb082f65f7aff29dcf2de
Peter R <peter.raback@csc.fi>
2026-04-21 14:22:47 +0300

    Fix for too long line.

--- fem/src/ParallelUtils.F90
+++ fem/src/ParallelUtils.F90
@@ -409,7 +409,8 @@ CONTAINS
               k = Matrix % Perm((i-1)*DOFs+j)
               IF ( k<=0 ) CYCLE
               IF (k > SIZE(Matrix % ParallelInfo % GlobalDOFs)) THEN
-                 CALL Fatal('ParallelInitMatrix','Matrix % ParallelInfo % GlobalDOFs bounds error. Matrix vs Solver perm scope conflict is a possible cause.')
+                CALL Fatal('ParallelInitMatrix','Matrix % ParallelInfo % GlobalDOFs bounds error.'//&
+                    ' Matrix vs Solver perm scope conflict is a possible cause.')
               END IF
               
               Matrix % ParallelInfo % GlobalDOFs(k) = &
