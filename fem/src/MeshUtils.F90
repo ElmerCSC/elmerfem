06e819d2bfa9905ae72e674e6108af1c1c3e421d
Mika Malinen <mika.malinen@csc.fi>
2026-05-06 14:04:26 +0300

    A slight distiction between DG and a generic construct "d:..."

--- fem/src/MeshUtils.F90
+++ fem/src/MeshUtils.F90
@@ -3321,7 +3321,7 @@ CONTAINS
      LOGICAL :: NeedEdges, Found, FoundDef0, FoundDef, FoundEq, GotIt, MeshDeps, &
          FoundEqDefs, FoundSolverDefs(Model % NumberOfSolvers), &
          FirstOrderElements, InheritDG, Hit, Stat, &
-         UpdateDefDofs(Model % NumberOfSolvers)
+         UpdateDefDofs(Model % NumberOfSolvers), DG
      TYPE(Element_t), POINTER :: Element, Parent, pParent
      TYPE(Element_t) :: DummyElement
      TYPE(ValueList_t), POINTER :: Vlist
@@ -3560,8 +3560,15 @@ CONTAINS
        EdgeDOFs(i) = MAX(0,inDOFs(el_id,2))
        FaceDOFs(i) = MAX(0,inDOFs(el_id,3))
 
+       IF (PRESENT(mySolver)) THEN
+         DG = Model % Solvers(mySolver) % DG
+       ELSE
+         DG = .FALSE.
+       END IF
+       
        IF ( inDofs(el_id,4) == 0 ) THEN
          inDOFs(el_id,4) = n
+         DG = .TRUE.
        END IF
 
        NULLIFY( Element % DGIndexes )
@@ -3575,7 +3582,15 @@ CONTAINS
          END DO
        END IF
        Element % DGDOFs = MAX(0,inDOFs(el_id,4))
-       NeedEdges = NeedEdges .OR. ANY( inDOFs(el_id,2:4)>0 )
+
+       IF (DG) THEN
+         NeedEdges = .TRUE.
+       ELSE
+         ! In the case of a non-DG solver the discontinuous DOFs might be created without
+         ! creating edge/face information: 
+         NeedEdges = NeedEdges .OR. ANY( inDOFs(el_id,2:3)>0 )
+       END IF
+!       NeedEdges = NeedEdges .OR. ANY( inDOFs(el_id,2:4)>0 )
        
        
        ! Check if given element is a p element
@@ -3822,6 +3837,9 @@ CONTAINS
      IF( Mesh % MaxEdgeDofs > 0 ) THEN
        CALL Info('NonNodalElements','Edge dofs max: '//I2S(Mesh % MaxEdgeDofs),Level=12)
      END IF
+     IF( Mesh % MaxBDofs > 0 ) THEN
+       CALL Info('NonNodalElements','Elementwise (bubble) dofs max: '//I2S(Mesh % MaxBDofs),Level=12)
+     END IF     
      IF( Mesh % MaxElementDofs > 0 ) THEN
        CALL Info('NonNodalElements','Element dofs max: '//I2S(Mesh % MaxElementDofs),Level=12)
      END IF
