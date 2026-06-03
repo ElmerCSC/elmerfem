e5b063f4c38934831b47937b3b2284bbb6d403b1
clemens_schannwell <56071417+ClemensSchannwell@users.noreply.github.com>
2026-06-03 11:19:14 +0200

    Update elmerice/Solvers/yac2elmer.F90
    Co-authored-by: Benjamin Rodenberg <rodenberg@dkrz.de>

--- elmerice/Solvers/yac2elmer.F90
+++ elmerice/Solvers/yac2elmer.F90
@@ -479,7 +479,10 @@ SUBROUTINE YAC2Elmer( Model,Solver,dt,TransientSimulation )
       ! TODO: stub implementation for ICON coupling
       ! CALL elmer_icon_interface(is_root_rank)
       ! cltVar => VariableGet( Mesh % Variables, 'tas' )
-      ! prVar => VariableGet( Mesh % Variables, 'pr_snow' )
+! TODO: stub implementation for ICON-A coupling
+! CALL elmer_icon_interface(is_root_rank)
+! cltVar => VariableGet( Mesh % Variables, 'tas' )
+! prVar => VariableGet( Mesh % Variables, 'pr_snow' )
   END IF
 
   CALL INFO(SolverName,'Coupling step done', Level=1)
